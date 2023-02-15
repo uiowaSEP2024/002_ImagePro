d41715788b23483f4b7817b6dda66d63ae935ec5
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-02-14 21:19:27 -0600

    Updated Datetime field in JSON and appends to txt

--- MockJson/mockscript.py
+++ MockJson/mockscript.py
@@ -1,16 +1,17 @@
 import uuid
 import json
 import time
+from datetime import datetime
 
 client_id = str(uuid.uuid4())
 job_id = str(uuid.uuid4())
 client_name = "RandomName"
 
 steps = 20
-json_begin = '{"objects":[\n'
-json_end = ']}'
+json_begin = '\n{"objects":[\n'
+json_end = ']},'
 
-with open("MockJson/sample.json", "w") as outfile:
+with open("MockJson/sample.txt", "a") as outfile:
     outfile.write(json_begin)
     for i in range(steps):
         time.sleep(3)
@@ -19,7 +20,8 @@ with open("MockJson/sample.json", "w") as outfile:
                 "client_id": client_id,
                 "job_id": job_id,
                 "client_name": client_name,
-                "step": i+1 
+                "step": i+1 ,
+                "job_time": str(datetime.now())
             },
             indent=4,
             # separators=(',',': ')
