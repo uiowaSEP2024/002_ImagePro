5cf143c0449b9a145df41061c75a1dd7a1ef7dee
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-02-14 21:43:44 -0600

    Updated script tp remove beggining and ending

--- MockJson/mockscript.py
+++ MockJson/mockscript.py
@@ -8,22 +8,22 @@ job_id = str(uuid.uuid4())
 client_name = "RandomName"
 
 steps = 20
-json_begin = '\n{"objects":[\n'
-json_end = ']},'
+# json_begin = '\n{"objects":[\n'
+# json_end = ']},'
 
 with open("MockJson/sample.txt", "a") as outfile:
-    outfile.write(json_begin)
+    # outfile.write(json_begin)
     for i in range(steps):
         time.sleep(3)
         json_object = json.dumps(
             {
-                "client_id": client_id,
-                "job_id": job_id,
-                "client_name": client_name,
-                "step": i+1 ,
-                "job_time": str(datetime.now())
+                "client_id": client_id, 
+                "job_id": job_id, 
+                "client_name": client_name, 
+                "step": i+1, 
+                "job_time": str(datetime.now())                
             },
-            indent=4,
+            indent=3,
             # separators=(',',': ')
         )
         outfile.write(json_object)
@@ -31,5 +31,5 @@ with open("MockJson/sample.txt", "a") as outfile:
             outfile.write(",")
         outfile.write("\n")
     
-    outfile.write(json_end)
+    # outfile.write(json_end)
         
