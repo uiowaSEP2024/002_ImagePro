7440c1f32a3dcbe8742e16fc96a21828592e1394
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-02-28 21:41:07 -0600

    Same

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,12 +26,12 @@ def read_root():
     return {"msg": "Hello World"}
 
 
-# @app.post("/users/", response_model=schemas.User)
-# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_by_email(db, email=user.email)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#     return crud.create_user(db=db, user=user)
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 
 # @app.get("/users/", response_model=list[schemas.User])
