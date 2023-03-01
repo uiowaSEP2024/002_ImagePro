88c5761f15511c1696217cda915b65b727201ba4
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-02-28 22:47:08 -0600

    Added Password hashing and restructered routers

---
+++ backend/app/routers/users.py
@@ -0,0 +1,31 @@
+from app import schemas
+from app.db import crud
+from app.db.database import SessionLocal, engine
+from app.db.models import users
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+users.Base.metadata.create_all(bind=engine)
+
+router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
+
+
+@router.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
--- backend/app/test_main.py
+++ backend/app/test_main.py
@@ -4,8 +4,20 @@ from .main import app
 
 client = TestClient(app)
 
+import json
+
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+def test_create_user():
+    data = {
+        "email": "testuser@nofoobar.com",
+        "password": "testing",
+    }
+    response = client.post("/users/", json=data)
+    assert response.status_code == 200
+    assert response.json()["email"] == "testuser@nofoobar.com"
