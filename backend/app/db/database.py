ad309e4c0644aa895612803a11fa601610d3414c
anandgopal22 <111823964+anandgopal22@users.noreply.github.com>
2023-03-01 11:50:29 -0600

    Update backend/app/db/database.py
    Co-authored-by: Rexford Essilfie <rexfordessilfie09@gmail.com>

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
