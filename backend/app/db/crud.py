e1d5565b69ba086b1c09009f95125760097f3bb8
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-02-28 21:41:01 -0600

    Added routes for getting users

--- backend/app/db/crud.py
+++ backend/app/db/crud.py
@@ -1,15 +1,15 @@
 from sqlalchemy.orm import Session
 
-from . import models
+from .models import users
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+    return db.query(users.User).filter(users.User.id == user_id).first()
 
 
 def create_user(db: Session): # user: schemas.UserCreate)
     # fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email="user.email", hashed_password="fake_hashed_password")
+    db_user = users.User(email="user.email", hashed_password="fake_hashed_password")
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,49 @@
-from typing import Union
+from typing import List
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from app.db import crud
+from app.db.database import SessionLocal, engine
+from app.db.models import users
+
+from . import schemas
+
+users.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
+
+
+# @app.post("/users/", response_model=schemas.User)
+# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#     db_user = crud.get_user_by_email(db, email=user.email)
+#     if db_user:
+#         raise HTTPException(status_code=400, detail="Email already registered")
+#     return crud.create_user(db=db, user=user)
+
+
+# @app.get("/users/", response_model=list[schemas.User])
+# def read_users(db: Session = Depends(get_db)):
+#     users = crud.get_user(db, 5)
+#     return [users]
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
