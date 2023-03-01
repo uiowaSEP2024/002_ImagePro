3e4243d9d60f311d62e6a9510d5fb01e3a5efe42
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-03-01 13:25:31 -0600

    Adding NextUI for nav

---
+++ frontend/src/pages/navbar.tsx
@@ -0,0 +1,26 @@
+import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+
+function TopNavbar() {
+    return (
+      <Navbar>
+        <Navbar.Content hideIn="xs">
+          <Navbar.Link href="#">Features</Navbar.Link>
+          <Navbar.Link isActive href="#">Customers</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Company</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="#">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat as={Link} href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+  );
+}
+
+export default TopNavbar;
