9db2a2ba5c9b26714e6c2f09eab99c869d76a058
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-03-01 13:25:31 -0600

    Adding NextUI for nav

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
+import TopNavbar from "./navbar"
+import Home from "./index"
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from '@nextui-org/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      < TopNavbar />
+      < Home />
+    </>
+  );
 }
+
+export default MyApp;
