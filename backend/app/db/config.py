9ae9fcb15d564387379303a35dce68ee22cc7f2f
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-02-28 22:47:08 -0600

    Added Password hashing and restructered routers

--- backend/app/db/config.py
+++ backend/app/db/config.py
@@ -3,18 +3,22 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-env_path = Path('.') / '.env'
+env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
+
 
 class Settings:
     # PROJECT_NAME:str = "Job Board"
     # PROJECT_VERSION: str = "1.0.0"
 
-    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER")
-    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
-    POSTGRES_DB : str = os.getenv("POSTGRES_DB")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_PORT: str = os.getenv(
+        "POSTGRES_PORT", 5432
+    )  # default postgres port is 5432
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
     DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,49 +1,13 @@
-from typing import List
+from fastapi import Depends, FastAPI
 
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
-
-from app.db import crud
-from app.db.database import SessionLocal, engine
-from app.db.models import users
-
-from . import schemas
-
-users.Base.metadata.create_all(bind=engine)
+from app.routers import users
 
 app = FastAPI()
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
-# @app.get("/users/", response_model=list[schemas.User])
-# def read_users(db: Session = Depends(get_db)):
-#     users = crud.get_user(db, 5)
-#     return [users]
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
+app.include_router(users.router)
