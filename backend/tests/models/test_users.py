e1023c73310f3f76ae8ed9d33a2fa48ec89f86a0
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-01 21:07:23 -0600

    Move tests to own directory

---
+++ backend/tests/models/test_users.py
@@ -0,0 +1,30 @@
+import pytest
+import sqlalchemy
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+
+from app.db.database import SessionLocal, ensure_tables_created
+from app.schemas import UserCreate
+from app.db.crud import create_user
+
+
+def test_users():
+    db = SessionLocal()
+    db_user = create_user(
+        db, UserCreate.parse_obj({"email": "jimbrown@example.com", "password": "abc"})
+    )
+
+    assert db_user.email == 'jimbrown@example.com'
+    assert db_user.hashed_password is not None
+
+
+def test_unique_user_email():
+    with pytest.raises(sqlalchemy.exc.IntegrityError):
+        db = SessionLocal()
+        create_user(
+            db, UserCreate.parse_obj({"email": "jimbrown@example.com", "password": "abc"})
+        )
+
+        create_user(
+            db, UserCreate.parse_obj({"email": "jimbrown@example.com", "password": "abc"})
+        )
---
+++ backend/tests/routers/test_main.py
@@ -0,0 +1,7 @@
+
+def test_read_main(app_client):
+    response = app_client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
+
