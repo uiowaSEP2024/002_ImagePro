e24dde2b36f5526dcc25c0bedaa32e994ab0c087
bainwala <hbainwala@gmail.com>
2023-03-02 11:49:05 -0600

    Changed link to signup page

--- frontend/src/pages/navbar.tsx
+++ frontend/src/pages/navbar.tsx
@@ -1,25 +1,25 @@
-import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
-
+import { Navbar, Button, Link } from "@nextui-org/react";
+ 
 function TopNavbar() {
-    return (
-      <Navbar>
-        <Navbar.Content hideIn="xs">
-          <Navbar.Link href="#">Features</Navbar.Link>
-          <Navbar.Link isActive href="#">Customers</Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Company</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content>
-          <Navbar.Link color="inherit" href="#">
+  return (
+    <Navbar>
+      <Navbar.Content hideIn="xs">
+        <Navbar.Link href="#">Features</Navbar.Link>
+        <Navbar.Link isActive href="#">Customers</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Company</Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Navbar.Link color="inherit" href="#">
             Login
-          </Navbar.Link>
-          <Navbar.Item>
-            <Button auto flat as={Link} href="#">
+        </Navbar.Link>
+        <Navbar.Item>
+          <Button auto flat as={Link} href="/signup">
               Sign Up
-            </Button>
-          </Navbar.Item>
-        </Navbar.Content>
-      </Navbar>
+          </Button>
+        </Navbar.Item>
+      </Navbar.Content>
+    </Navbar>
   );
 }
 
