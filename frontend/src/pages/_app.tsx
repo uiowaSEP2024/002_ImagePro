214ca7e2778ec85080d56bcc8937758f18980675
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-02 13:46:51 -0600

    Relocate navbar plus fix links not rendering

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,16 @@
-import "@/styles/globals.css"
-import type { AppProps } from "next/app"
-import TopNavbar from "./navbar"
-import Home from "./index"
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "src/components/Navbar";
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
-      < TopNavbar />
-      < Home />
+      {/* Render the Navbar */}
+      <Navbar />
+      {/* Render the page */}
+      <Component {...pageProps} />
     </NextUIProvider>
   );
 }
