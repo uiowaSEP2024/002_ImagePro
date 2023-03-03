2c79bf9244271672d658a16144bf18524a702f02
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-03 12:46:30 -0600

    Setup backend middleware to receive frontend requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.models.base import ensure_tables_created
 from app.routers import users_router
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
