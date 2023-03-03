06c6cb44c9e6bb153351e6d8459c906f65aeb5dd
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-03-03 12:09:28 -0600

    commiting pages for navbar and testing files

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,47 +1,94 @@
-import { Button, Link, Spacer, Text, Textarea, Container, Col, Row, Grid } from "@nextui-org/react";
+import React from 'react';
+import {
+  Card,
+  Spacer,
+  Button,
+  Text,
+  Input,
+  Row,
+  Link,
+  Container,
+} from '@nextui-org/react';
 
 export default function SignUp() {
   return (
-    <>
-      <Grid.Container gap={2} justify="center" >
-        <Row>
-          <Col/>
-          <Col>
-            <Spacer y={1} />
-            <Text h3>Create Account</Text>
-            <Spacer y={1}/>
-            <Container justify="center" >
-              <Textarea
-                underlined
-                color="primary"
-                label="First Name"
-              />
-              <Spacer y={1}/>
-              <Textarea
-                underlined
-                color="primary"
-                label="Last Name"
-              />
-              <Spacer y={1} />
-              <Textarea
-                underlined
-                color="primary"
-                label="Email"
-              />
-            </Container>
-            <Spacer y={1}/>
-            <Button flat color="primary" auto>
-              Sign Up
-            </Button>
-            <Spacer y={1}/>
-            <Link block color="secondary" href="/login">
-              Existing user? Go to Login
-            </Link>
-          </Col>
-          <Col/>
-          <Col/>
-        </Row>
-      </Grid.Container>
-    </>
-  )
+    <div>
+      <Container
+        display="flex"
+        alignItems="center"
+        justify="center"
+        css={{ minHeight: '100vh' }}
+      >
+        <Card css={{ mw: '420px', p: '20px' }} variant="bordered">
+          <Text
+            size={24}
+            weight="bold"
+            css={{
+              as: 'center',
+              mb: '20px',
+            }}
+          >
+            Sign Up
+          </Text>
+          <Input
+            clearable
+            underlined
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="First Name"
+            aria-label="First Name"
+          />
+          <Spacer y={1} />
+          <Input
+            clearable
+            underlined
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Last Name"
+            aria-label="Last Name"
+          />
+           <Spacer y={1} />
+          <Input
+            clearable
+            underlined
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Email"
+            aria-label="Email"
+            css={{ mb: '6px' }}
+          />
+           <Spacer y={1} />
+          <Input
+            clearable
+            underlined
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Password"
+            aria-label="Password"
+            css={{ mb: '6px' }}
+          />
+          <Spacer y={1} />
+          <Input
+            clearable
+            underlined
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Confirm Password"
+            aria-label="Confirm Password"
+            css={{ mb: '6px' }}
+          />
+          <Row justify="space-between">
+          <Link block color="secondary" href="/login">Existing user? Log in. </Link>
+          </Row>
+          <Spacer y={1} />
+          <Button>Create Account</Button>
+        </Card>
+      </Container>
+    </div>
+  );
 }
