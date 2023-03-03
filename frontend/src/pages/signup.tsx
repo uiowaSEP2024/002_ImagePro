d72b021dd471a59780b7d9f5bb6ef1cfd132c182
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-03-02 20:45:28 -0600

    Adding signup NextUI styling and navbar links. Tests failing due to prediscussed error

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,5 +1,46 @@
+import { Button, Link, Spacer, Text, Textarea, Container, Col, Row } from "@nextui-org/react";
+
 export default function SignUp() {
   return (
-    <h1>This is the signup page</h1>
+    <Container align-content="center" >
+      <Row>
+        <Col/>
+        <Col>
+          <Spacer y={1} />
+          <Text h3>Create Account</Text>
+          <Spacer y={1}/>
+          <Container justify="center" >
+          <Textarea
+              underlined
+              color="primary"
+              labelPlaceholder="First Name"
+            />
+          <Spacer y={1}/>
+          <Textarea
+              underlined
+              color="primary"
+              labelPlaceholder="Last Name"
+            />
+          <Spacer y={1} />
+          <Textarea
+              underlined
+              color="primary"
+              labelPlaceholder="Email"
+            />
+          </Container>
+          <Spacer y={1}/>
+          <Button flat color="primary" auto>
+            Sign Up
+          </Button>
+          <Spacer y={1}/>
+          <Link block color="secondary" href="#">
+            Existing user? Go to Login
+          </Link>
+        </Col>
+        <Col/>
+        <Col/>
+      </Row>
+    </Container>
+
   )
 }
