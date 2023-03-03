cb286c105066c2dbf5828c6bcd1ccc876625404a
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-02 21:49:43 -0600

    Testing tweaks plus disable dark-mode

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,19 +1,19 @@
 import { Navbar, Button, Link, useTheme } from "@nextui-org/react";
- 
+
 function TopNavbar() {
   return (
-    <Navbar variant="sticky" >
-      <Navbar.Content enableCursorHighlight hideIn="xs" >
-        <Navbar.Link isActive href="../index">Home</Navbar.Link>
+    <Navbar variant="sticky">
+      <Navbar.Content enableCursorHighlight hideIn="xs">
+        <Navbar.Link isActive href="/">
+          Home
+        </Navbar.Link>
         <Navbar.Link href="#">Dashboard</Navbar.Link>
         <Navbar.Link href="#">Billing</Navbar.Link>
       </Navbar.Content>
       <Navbar.Content enableCursorHighlight hideIn="xs">
-        <Navbar.Link href="#">
-            Login
-        </Navbar.Link>
-        <Navbar.Link isActive href="../signup">
-            Sign up
+        <Navbar.Link href="#">Login</Navbar.Link>
+        <Navbar.Link isActive href="/signup">
+          Sign up
         </Navbar.Link>
       </Navbar.Content>
     </Navbar>
