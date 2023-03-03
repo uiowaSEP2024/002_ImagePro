8bfa0222334dbe9d050fd1a270775d686c0405e0
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-02 22:08:55 -0600

    Temporarily remove isActives

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,17 +4,13 @@ function TopNavbar() {
   return (
     <Navbar variant="sticky">
       <Navbar.Content enableCursorHighlight hideIn="xs">
-        <Navbar.Link isActive href="/">
-          Home
-        </Navbar.Link>
+        <Navbar.Link href="/">Home</Navbar.Link>
         <Navbar.Link href="#">Dashboard</Navbar.Link>
         <Navbar.Link href="#">Billing</Navbar.Link>
       </Navbar.Content>
       <Navbar.Content enableCursorHighlight hideIn="xs">
         <Navbar.Link href="#">Login</Navbar.Link>
-        <Navbar.Link isActive href="/signup">
-          Sign up
-        </Navbar.Link>
+        <Navbar.Link href="/signup">Sign up</Navbar.Link>
       </Navbar.Content>
     </Navbar>
   );
