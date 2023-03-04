f2a40ea2b878f3947066ad2fec42c561e6c1c39d
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-04 14:10:30 -0600

    Setup random test user for tests

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,7 +1,11 @@
+import random
+
 from tasks import setup_app_settings
 
+# NB: this should happen before any app imports to ensure the environment is set
 setup_app_settings("test")
 
+from app import services, schemas
 import pytest
 from app.models.base import truncate_all_tables
 from fastapi.testclient import TestClient
@@ -25,3 +29,13 @@ def db():
     from config.database import SessionLocal
 
     return SessionLocal()
+
+
+@pytest.fixture
+def random_test_user(db):
+    random_tag = random.randint(0, 10000)
+    test_user = services.create_user(
+        db,
+        schemas.UserCreate(email=f"testuser_{random_tag}@example.com", password="abc"),
+    )
+    return test_user
