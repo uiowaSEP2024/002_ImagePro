14e8e5015db25482a0ca3d722dba7e2501bcf038
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-04 12:07:12 -0600

    Setup initial api key modules

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,2 +1,2 @@
-from .users import create_user, get_user
-from .apikeys import create_apikey_for_user, get_user_apikeys
+from .users import *
+from .api_keys import *
--- backend/app/services/api_keys.py
+++ backend/app/services/api_keys.py
@@ -12,6 +12,10 @@ def get_user_apikeys(db: Session, user_id: int):
     return get_user(db, user_id).apikeys
 
 
+def get_user_from_api_key(db: Session, api_key: str):
+    return db.query(models.Apikey).filter(models.Apikey.id == api_key).first().user
+
+
 def create_apikey_for_user(db: Session, user_id: int):
     db_apikey = models.Apikey(key=generate_apikey(), user_id=user_id)
 
