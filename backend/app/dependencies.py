d7bee48cf5f616cbf2d4a20adf03d90a36fe0b49
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-04 19:46:48 -0600

    Resolve PR comments

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,3 +1,18 @@
+from fastapi.security import OAuth2
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi import Request, Depends
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi import HTTPException
+from fastapi import status
+from typing import Optional
+from typing import Dict
+
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+from app.services.users import get_user_by_email
+from config import settings
 from config.database import SessionLocal
 
 
@@ -7,3 +22,69 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+class OAuth2HttpCookieBearer(OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: Optional[str] = None,
+        scopes: Optional[Dict[str, str]] = None,
+        description: Optional[str] = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
+        super().__init__(
+            flows=flows,
+            scheme_name=scheme_name,
+            description=description,
+            auto_error=auto_error,
+        )
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization = request.cookies.get("access_token")
+        scheme, param = get_authorization_scheme_param(authorization)
+
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return param
+
+
+oauth2_http_cookie_bearer_scheme = OAuth2HttpCookieBearer(tokenUrl="login")
+
+
+def get_current_user_from_token(
+    db: Session = Depends(get_db),
+    token: str = Depends(oauth2_http_cookie_bearer_scheme),
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+
+    try:
+        payload = jwt.decode(
+            token, settings.secret_key, algorithms=[settings.algorithm]
+        )
+        email: str = payload.get("sub")
+
+        if email is None:
+            raise credentials_exception
+
+        user = get_user_by_email(email=email, db=db)
+
+        if user is None:
+            raise credentials_exception
+
+        return user
+    except JWTError:
+        raise credentials_exception
