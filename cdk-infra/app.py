66be85909cd328e07d2ea8b3cae7f308da993803
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-18 15:47:06 -0500

    Setup testing for CDK deployment

--- cdk-infra/app.py
+++ cdk-infra/app.py
@@ -5,50 +5,34 @@ import aws_cdk as cdk
 
 from cdk_infra.cdk_infra_stack import CdkInfraStack
 
-
 from models import BuildConfig
 
-app = cdk.App()
 
-
-def get_config_for_app_env():
-    app_env: str = app.node.try_get_context('app_env')
+def get_config_for_app_env(cdk_app: cdk.App, app_env=None):
+    app_env: str = app_env if app_env else cdk_app.node.try_get_context('app_env')
 
     if not app_env:
         raise Exception("Context variable missing on CDK command. Pass in as `-c app_env=development|production`")
 
-    default_config: dict = app.node.try_get_context('default')
-    raw_config: dict = app.node.try_get_context(app_env)
+    default_config: dict = cdk_app.node.try_get_context('default')
+    app_env_config: dict = cdk_app.node.try_get_context(app_env)
 
-    return BuildConfig.parse_obj({**default_config, **raw_config})
+    print(default_config, app_env_config)
+
+    return BuildConfig.parse_obj({**default_config, **app_env_config})
 
 
 def main():
-    build_config = get_config_for_app_env()
+    app = cdk.App()
+
+    build_config = get_config_for_app_env(app)
 
     cdk.Tags.of(app).add('AppName', build_config.AppName)
     cdk.Tags.of(app).add('AppEnv', build_config.AppEnv)
 
     stack_name = '-'.join([build_config.AppName, build_config.AppEnv])
 
-    CdkInfraStack(app, stack_name,
-                  build_config=build_config
-                  # If you don't specify 'env', this stack will be environment-agnostic.
-                  # Account/Region-dependent features and context lookups will not work,
-                  # but a single synthesized template can be deployed anywhere.
-
-                  # Uncomment the next line to specialize this stack for the AWS Account
-                  # and Region that are implied by the current CLI configuration.
-
-                  # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-                  # Uncomment the next line if you know exactly what Account and Region you
-                  # want to deploy the stack to. */
-
-                  # env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-                  # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                  )
+    CdkInfraStack(app, stack_name, build_config=build_config)
 
     app.synth()
 
--- cdk-infra/tests/unit/test_cdk_infra_stack.py
+++ cdk-infra/tests/unit/test_cdk_infra_stack.py
@@ -2,14 +2,36 @@ import aws_cdk as core
 import aws_cdk.assertions as assertions
 
 from cdk_infra.cdk_infra_stack import CdkInfraStack
+from app import get_config_for_app_env
+import pytest
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in cdk_infra/cdk_infra_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = CdkInfraStack(app, "cdk-infra")
+import json
+
+with open("cdk.json") as fp:
+    context = json.load(fp)["context"]
+
+
+def test_stack_created():
+    app = core.App(context=context)
+
+    build_config = get_config_for_app_env(app, app_env='test')
+
+    stack = CdkInfraStack(app, "test-cdk-infra", build_config)
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    template.has_resource_properties("AWS::Lambda::Function", {
+        "FunctionName": "Team8CDKFunctionTest",
+        "PackageType": "Image",
+        "Environment": {
+            "Variables": {
+                "APP_ENV": "test"
+            }
+        }
+    })
+
+    template.has_resource("AWS::ApiGateway::RestApi", {})
+
+    template.has_resource_properties("AWS::ApiGateway::Stage", {
+        "StageName": "test"
+    })
+    
