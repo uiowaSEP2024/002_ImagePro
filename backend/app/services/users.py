e85979babda7d097bd662c5ee8a7d71a6b40b5d1
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-03-20 20:25:58 -0500

    Added modifications to db and models

--- backend/app/services/users.py
+++ backend/app/services/users.py
@@ -18,6 +18,8 @@ def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(
         email=user.email, 
         hashed_password=get_password_hash(user.password),
+        first_name=user.first_name,
+        last_name=user.last_name
     )
     db.add(db_user)
     db.commit()
