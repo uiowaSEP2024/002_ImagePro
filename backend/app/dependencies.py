3f83ddc253a5c99a32f8469a704c96cef0eb3b97
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-03-21 15:03:27 -0500

    Navbar logic complete

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -57,19 +57,15 @@ class OAuth2HttpCookieBearer(OAuth2):
         )
 
     async def __call__(self, request: Request) -> Optional[str]:
-        # print("Cookies: ", request.cookies)
         authorization = request.cookies.get("access_token")
         scheme, param = get_authorization_scheme_param(authorization)
-        # print("Auth:", authorization)
 
         if not authorization or scheme.lower() != "bearer":
             if self.auto_error:
-                # print("Not Auth")
                 raise HTTPException(
                     status_code=HTTP_401_UNAUTHORIZED,
                     detail="Not authenticated",
                     headers={"WWW-Authenticate": "Bearer"},
-                    # print("Not Auth")
                 )
             else:
                 return None
