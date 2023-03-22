5d2ff5c55e0223c4ec5a07a83947bc811773e93a
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-03-22 18:00:42 -0500

    Changing billing to be a protected route, current issue with fetch

--- frontend/src/pages/billing.tsx
+++ frontend/src/pages/billing.tsx
@@ -1,13 +1,40 @@
 import { Text, Grid } from "@nextui-org/react";
+import { useState, useEffect } from 'react';
+import { useRouter } from "next/router";
 
 export default function Billing() {
-  return (
-    <>
-      <Grid.Container gap={2} justify="center">
-        <Text h1 align-items="center">
-          Billing page
-        </Text>
-      </Grid.Container>
-    </>
-  );
+  const router = useRouter();
+  const [data, setData] = useState(null)
+
+  const redirect = () => {
+    router.push("/login")
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:8000/login", {
+      credentials: "include",
+      method: "GET",
+    }
+    ).then((result) => result.json()).then((data) => {
+      setData(data.detail)
+      console.log(data.detail)
+    })
+  })
+
+  if (data == "Not authenticated") {
+    return (
+      redirect()
+    );
+  } else {
+    return (
+      <>
+        <Grid.Container gap={2} justify="center">
+          <Text h1 align-items="center">
+            Billing page
+          </Text>
+        </Grid.Container>
+      </>
+    );
+  }
+  
 }
