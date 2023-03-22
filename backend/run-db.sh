aaa306d268062e65735860d2c194aa27ead29bdb
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-22 01:51:55 -0500

    Add script for starting docker postgres database

---
+++ backend/run-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Stop and remove existing containers
+echo "===>Stopping existing backend-postgres containers"
+docker stop backend-postgres &> /dev/null || true
+docker rm backend-postgres &> /dev/null || true
+
+# Start a new docker instance with postgres image
+echo "===>Starting new backend-postgres container"
+docker run --name backend-postgres \
+  --env-file=.env.local \
+  -p 5432:5432 \
+  -v pg_data:/var/lib/postgresql/data \
+  --rm \
+  postgres:14
