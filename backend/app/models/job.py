a5ae09ffc4f15dbc23bf071bfc08299f03513a09
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-22 19:27:55 -0500

    Add provider_id column to the Job model

--- backend/app/models/job.py
+++ backend/app/models/job.py
@@ -11,13 +11,17 @@ class Job(Base):
     # Auto-generated internal job id
     id = Column(Integer, primary_key=True, index=True)
 
-    # The job_id as provided by the client
-    client_job_id = Column(String, index=True, nullable=False)
+    # The person sending the jobs
+    provider_id = Column(Integer, ForeignKey("users.id"), index=True, nullable=False)
+    provider = relationship("User", back_populates="jobs", foreign_keys=[provider_id])
 
-    # The job_kind as specified by the client e.g. ProstateV1, KidneyV2
+    # The job_id as provided by the provider
+    provider_job_id = Column(String, index=True, nullable=False)
+
+    # The job_name as specified by the provider e.g. ProstateV1, KidneyV2
     # May be used to set up billing or simply display information later.
-    client_job_kind = Column(String, index=True, nullable=False)
+    provider_job_name = Column(String, index=True, nullable=False)
 
     # The customer that this job belongs to. Must be a user in the system
     customer_id = Column(Integer, ForeignKey("users.id"), index=True, nullable=False)
-    customer = relationship("User", back_populates="jobs")
+    customer = relationship("User", back_populates="jobs", foreign_keys=[customer_id])
