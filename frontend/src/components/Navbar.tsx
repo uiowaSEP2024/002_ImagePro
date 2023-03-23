fe972201d0b3808febaf1e1b00d7cacdb427bccd
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-03-23 12:54:34 -0500

    Modified Profile page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -48,7 +48,7 @@ function TopNavbar() {
           <Navbar.Link href="/billing">Billing</Navbar.Link>
         </Navbar.Content>
         <Navbar.Content enableCursorHighlight hideIn="xs">
-        <Navbar.Link href="/profile">Profile</Navbar.Link>
+        <Navbar.Link href="/profile">My Profile</Navbar.Link>
         <Button flat light color="default" onPress={sendLogOutReq}>Log Out</Button>
         </Navbar.Content>
       </Navbar>
--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -3,23 +3,26 @@ import { Text, Grid } from "@nextui-org/react";
 import { useState, useEffect } from 'react';
 
 export default function Profile() {
-  const [data, setData] = useState(null)
-
+  const [first_name, setFirst_Name] = useState(null)
+  const [last_name, setLast_Name] = useState(null)
+  const [email, setEmail] = useState(null)
   useEffect(() => {
     fetch("http://localhost:8000/login", {
       credentials: "include",
       method: "GET",
     }
     ).then((result) => result.json()).then((data) => {
-      setData(data.user.first_name)
-      
+      setFirst_Name(data.user.first_name)
+      setLast_Name(data.user.last_name)
+      setEmail(data.user.email)
     })
   })
   return (
     <>
       <Grid.Container gap={2} justify="center">
         <Text h1 align-items="center">
-          Profile for { data }
+          Profile for { last_name }, { first_name }
+          Email: {email}
         </Text>
       </Grid.Container>
     </>
