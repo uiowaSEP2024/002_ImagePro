df487483a95831893727fdd852fc682fccdd60e7
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-22 22:49:06 -0500

    Add events model

---
+++ backend/app/models/event.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String, Integer, Enum
+from sqlalchemy.orm import relationship
+
+from .base import Base
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    # Auto-generated internal event id
+    id = Column(Integer, primary_key=True, index=True)
+
+    # The job id the events correspond to
+    job_id = Column(Integer, ForeignKey("jobs.id"), index=True, nullable=False)
+    job = relationship("Job", back_populates="events", foreign_keys=[job_id])
+
+    # The event_name as specified by the provider e.g. "Scanning X"
+    name = Column(String, nullable=True)
+
+    # The kind of event (info, step, error, etc.)
+    kind = Column(
+        Enum("step", "error", "info", "complete", name="event_kind"),
+        nullable=False,
+    )
+
+    # TODO: metadata column for events
