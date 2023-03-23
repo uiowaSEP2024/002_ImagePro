c0c7054f3f8c59cfb857f2d86c49106ed0511617
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-22 19:32:39 -0500

    Add tests for the Job model

--- backend/tests/models/test_jobs.py
+++ backend/tests/models/test_jobs.py
@@ -5,30 +5,42 @@ import sqlalchemy
 from app import models
 
 
-def test_create_job(db, random_test_user):
+def test_create_job(db, random_test_user, random_provider_user):
     job = models.Job(
-        client_job_id="abc123",
-        client_job_kind="kidneyV1",
+        provider_job_id="abc123",
+        provider_job_name="kidneyV1",
         customer_id=random_test_user.id,
+        provider_id=random_provider_user.id,
     )
 
     db.add(job)
     db.commit()
+
     db.refresh(job)
 
     assert isinstance(job, models.Job)
     assert job.customer_id == random_test_user.id
-    assert job.client_job_id == "abc123"
-    assert job.client_job_kind == "kidneyV1"
+    assert job.provider_job_id == "abc123"
+    assert job.provider_job_name == "kidneyV1"
 
     db.refresh(random_test_user)
+    # Check that the customer user now has the correct jobs associated with them
     assert len(random_test_user.jobs) == 1
+    assert random_test_user.jobs[0].provider_job_id == "abc123"
+    assert random_test_user.jobs[0].customer_id == random_test_user.id
+
+    db.refresh(random_provider_user)
+    # Check that the provider user now has the correct jobs associated with them
+    assert len(random_provider_user.provider_jobs) == 1
+    assert random_provider_user.provider_jobs[0].provider_job_id == "abc123"
+    assert random_provider_user.provider_jobs[0].provider_id == random_provider_user.id
 
 
-def test_create_job_missing_customer_id(db, random_test_user):
+def test_create_job_missing_customer_id(db, random_test_user, random_provider_user):
     job = models.Job(
-        client_job_id="abc123",
-        client_job_kind="kidneyV1",
+        provider_job_id="abc123",
+        provider_job_name="kidneyV1",
+        provider_id=random_provider_user.id,
     )
 
     db.add(job)
@@ -41,10 +53,11 @@ def test_create_job_missing_customer_id(db, random_test_user):
     assert "customer_id" in str(exc.value.orig)
 
 
-def test_create_job_missing_client_job_id(db, random_test_user):
+def test_create_job_missing_provider_job_id(db, random_test_user, random_provider_user):
     job = models.Job(
+        provider_job_name="kidneyV1",
         customer_id=random_test_user.id,
-        client_job_kind="kidneyV1",
+        provider_id=random_provider_user.id,
     )
 
     db.add(job)
@@ -54,4 +67,23 @@ def test_create_job_missing_client_job_id(db, random_test_user):
 
     # Check for null violation and that column is part of the error message in the error
     assert isinstance(exc.value.orig, psycopg2.errors.NotNullViolation)
-    assert "client_job_id" in str(exc.value.orig)
+    assert "provider_job_id" in str(exc.value.orig)
+
+
+def test_create_job_missing_provider_job_name(
+    db, random_test_user, random_provider_user
+):
+    job = models.Job(
+        provider_job_id="abc123",
+        customer_id=random_test_user.id,
+        provider_id=random_provider_user.id,
+    )
+
+    db.add(job)
+
+    with pytest.raises(sqlalchemy.exc.IntegrityError) as exc:
+        db.commit()
+
+    # Check for null violation and that column is part of the error message in the error
+    assert isinstance(exc.value.orig, psycopg2.errors.NotNullViolation)
+    assert "provider_job_name" in str(exc.value.orig)
