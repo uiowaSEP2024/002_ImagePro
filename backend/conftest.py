2684ee3737d74113da36fed7af51f700b97670b8
bainwala <hbainwala@gmail.com>
2023-03-24 12:19:36 -0500

    Increased random probability of getting unique users

--- backend/conftest.py
+++ backend/conftest.py
@@ -5,10 +5,9 @@ from tasks import setup_app_settings
 # NB: this should happen before any app imports to ensure the environment is set
 setup_app_settings("test")
 
-from app import services, schemas
 import pytest
+from app import schemas, services
 from app.models.base import truncate_all_tables
-
 from fastapi.testclient import TestClient
 
 
@@ -34,7 +33,7 @@ def db():
 
 @pytest.fixture
 def random_test_user(db):
-    random_tag = random.randint(0, 10000)
+    random_tag = random.randint(0, 1000000)
     test_user = services.create_user(
         db,
         schemas.UserCreate(email=f"testuser_{random_tag}@example.com", password="abc"),
