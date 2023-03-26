b179028edb742bdcb21dc835bc6dc86539a29b5c
ryan-edwall <111799742+ryan-edwall@users.noreply.github.com>
2023-03-25 21:50:52 -0500

    TEST: Implemented create event test and job fixture

---
+++ backend/tests/routers/test_events.py
@@ -0,0 +1,24 @@
+from app import services
+from app.dependencies import API_KEY_HEADER_NAME
+from app import schemas, services
+
+def test_create_event(app_client, job_for_random_user_with_api_key):
+
+    job = job_for_random_user_with_api_key
+    data = {
+        "kind": "step",
+        "name": job.provider_job_name,
+        "provider_job_id": job.provider_job_id
+    }
+    response = app_client.post(
+        "/events",
+        json=data,
+        headers={API_KEY_HEADER_NAME: job_for_random_user_with_api_key.provider.api_keys[0].key},
+    )
+
+    assert response.status_code == 200
+    assert response.json()["kind"] == "step"
+    assert response.json()["name"] == "Scanning"
+    assert response.json()["id"] == job.id
+    assert response.json()["job_id"] == job.provider_id
+
