f2c1c2ff025c136780fadcde2bf97b12cc033eef
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-03-25 23:17:06 -0500

    pushing changes to omit errors in testing files

--- frontend/__tests__/login.test.tsx
+++ frontend/__tests__/login.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen, RenderResult } from "@testing-library/react";
+import 'core-js/es/modules/es.promise';
 import Login from "@/pages/login";
 import "@testing-library/jest-dom";
 
 jest.mock("next/router", () => require("next-router-mock"));
 let documentBody: RenderResult;
 
-
 // TODO: explore fixing snapshot testing with https://github.com/mui/material-ui/issues/21293#issuecomment-654921524
 // https://www.pluralsight.com/guides/how-to-test-react-components-in-typescript
 describe("Login", () => {
+
+  beforeAll(() => {
+    (global as any).fetch = fetch as (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+  });
 
   beforeEach(() => {
     // Arrange
--- frontend/__tests__/signup.test.tsx
+++ frontend/__tests__/signup.test.tsx
@@ -2,9 +2,16 @@ import { render, screen } from "@testing-library/react";
 import Signup from "@/pages/signup";
 import "@testing-library/jest-dom";
 
+jest.mock('next/router', () => require('next-router-mock'));
+
 // TODO: explore fixing snapshot testing with https://github.com/mui/material-ui/issues/21293#issuecomment-654921524
 describe("Signup", () => {
-  xit("renders Signup unchanged", () => {
+  it("renders Signup unchanged", () => {
+
+    beforeAll(() => {
+      (global as any).fetch = fetch as (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+    });
+
     const { container } = render(<Signup />);
     // expect(container).toMatchSnapshot();
   });
