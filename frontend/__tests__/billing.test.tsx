ce8077fd96de4930ed62fb96a33c9c76cbb4100b
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-03-26 18:05:57 -0500

    adding fetch mock to billing testing

--- frontend/__tests__/billing.test.tsx
+++ frontend/__tests__/billing.test.tsx
@@ -1,25 +1,61 @@
 import { render, screen } from "@testing-library/react";
 import Billing from "@/pages/billing";
 import "@testing-library/jest-dom";
-import fetch from 'node-fetch';
-
+import fetch, { Response as FetchResponse } from 'node-fetch';
 
 
 jest.mock('next/router', () => require('next-router-mock'));
+jest.mock('node-fetch', () => jest.fn());
 
-// TODO: explore fixing snapshot testing with https://github.com/mui/material-ui/issues/21293#issuecomment-654921524
 describe("Billing", () => {
-
-  beforeAll(() => {
-    (global as any).fetch = fetch as (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+  beforeEach(() => {
+    jest.resetAllMocks();
   });
 
-  it("renders a heading", () => {
+  it("renders a heading", async () => {
+
+    type ResponseBody = {
+      data: any[];
+    };
+    
+    const responseBody: ResponseBody = { data: [] };
+
+    const blob = new Blob([JSON.stringify(responseBody)], { type: 'application/json' });
+
+    const formData = new FormData();
+    formData.append('data', blob);
+
+    const response = new Response(blob, {
+      status: 200,
+      headers: { 'Content-type': 'application/json' },
+    });
+
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce(response);
+
     render(<Billing />);
+
     const heading = screen.getByRole("heading", {
       name: /Billing page/i,
     });
-    expect(heading).toBeInTheDocument();
-  });
 
+    expect(heading).toBeInTheDocument();
+
+    // Assert that the fetch function was called with the expected parameters
+    expect(fetch).toHaveBeenCalledWith('/api/billing', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  });
 });
+
+//  it("renders a heading", () => {
+//    render(<Billing />);
+//    const heading = screen.getByRole("heading", {
+//      name: /Billing page/i,
+//    });
+//    expect(heading).toBeInTheDocument();
+//  });
+
+//});
