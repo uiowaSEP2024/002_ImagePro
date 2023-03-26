1f55e36384a7ee92cfed8ac5ea77f90962f9b758
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-03-25 23:17:06 -0500

    pushing changes to omit errors in testing files

--- frontend/__tests__/billing.test.tsx
+++ frontend/__tests__/billing.test.tsx
@@ -2,8 +2,16 @@ import { render, screen } from "@testing-library/react";
 import Billing from "@/pages/billing";
 import "@testing-library/jest-dom";
 
+
+jest.mock('next/router', () => require('next-router-mock'));
+
 // TODO: explore fixing snapshot testing with https://github.com/mui/material-ui/issues/21293#issuecomment-654921524
 describe("Billing", () => {
+
+  beforeAll(() => {
+    (global as any).fetch = fetch as (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+  });
+
   it("renders a heading", () => {
     render(<Billing />);
     const heading = screen.getByRole("heading", {
@@ -12,8 +20,4 @@ describe("Billing", () => {
     expect(heading).toBeInTheDocument();
   });
 
-  xit("renders Billing unchanged", () => {
-    const { container } = render(<Billing />);
-    // expect(container).toMatchSnapshot();
-  });
 });
--- frontend/__tests__/dashboard.test.tsx
+++ frontend/__tests__/dashboard.test.tsx
@@ -1,11 +1,16 @@
 import { render, screen, RenderResult } from "@testing-library/react";
 import Dashboard from "@/pages/dashboard";
 import "@testing-library/jest-dom";
-
 let documentBody: RenderResult;
+
+jest.mock('next/router', () => require('next-router-mock'));
 
 // TODO: explore fixing snapshot testing with https://github.com/mui/material-ui/issues/21293#issuecomment-654921524
 describe("Dashboard", () => {
+
+  beforeAll(() => {
+    (global as any).fetch = fetch as (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+  });
 
   beforeEach(() => {
     // Arrange
