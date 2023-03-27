f17500587d82493ca87f2394847c985b76763263
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-03-26 20:17:53 -0500

    Backend linting

--- backend/app/services/users.py
+++ backend/app/services/users.py
@@ -16,10 +16,10 @@ def get_user_by_email(db: Session, email: str):
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(
-        email=user.email, 
+        email=user.email,
         hashed_password=get_password_hash(user.password),
         first_name=user.first_name,
-        last_name=user.last_name
+        last_name=user.last_name,
     )
     db.add(db_user)
     db.commit()
