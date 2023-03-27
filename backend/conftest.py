8e82dff91a82f3cdfc52c3bde5fe9cd8e2de4675
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-03-26 23:37:57 -0500

    Model fix on test

--- backend/conftest.py
+++ backend/conftest.py
@@ -94,7 +94,7 @@ def random_test_user_factory(db):
             test_user = services.create_user(
                 db,
                 schemas.UserCreate(
-                    email=f"testuser_{random_tag}@example.com", password="abc"
+                    email=f"testuser_{random_tag}@example.com", password="abc", first_name="first", last_name="last"
                 ),
             )
             return test_user
