dc40ec878fcb97de33eae596bc5a0677462dfe04
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-03-31 13:02:42 -0500

    refactor: move trackerapi.py to sub package

--- trackerapi/api/trackerapi.py
+++ trackerapi/api/trackerapi.py
@@ -7,9 +7,9 @@ class TrackerAPI:
     to create jobs, and send events.
     """
 
-    DEFAULT_BASE_URL = 'http://localhost:8000'
+    DEFAULT_BASE_URL = "http://localhost:8000"
 
-    def __init__(self, api_key=None, base_url=None):
+    def __init__(self, api_key, base_url=None):
         self.api_key = api_key
         self.base_url = base_url if base_url else TrackerAPI.DEFAULT_BASE_URL
 
@@ -21,11 +21,14 @@ class TrackerAPI:
         return response.json()
 
     def __get_request(self, url):
-        return self.__to_json(requests.get(url,
-                                           headers=self.__get_headers()))
+        return self.__to_json(requests.get(url, headers=self.__get_headers()))
 
     def __post_request(self, url, data):
-        return self.__to_json(requests.post(url, json=data, headers=self.__get_headers()))
+        return self.__to_json(
+            requests.post(url, json=data, headers=self.__get_headers())
+        )
 
     def __patch_request(self, url, data):
-        return self.__to_json(requests.patch(url, json=data, headers=self.__get_headers()))
+        return self.__to_json(
+            requests.patch(url, json=data, headers=self.__get_headers())
+        )
