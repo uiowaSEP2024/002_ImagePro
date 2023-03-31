1269cee90475963d5b8084dc8f1947efa647b88b
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-03-31 12:25:08 -0500

    Able to generate API keys successfully from NextUI

--- frontend/src/pages/apikeys.tsx
+++ frontend/src/pages/apikeys.tsx
@@ -3,17 +3,17 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { checkUserLoggedIn } from "@/utils/auth";
 import { Text } from "@nextui-org/react";
-import { Card, Container, Link } from "@nextui-org/react";
+import { Container, Row, Col, Spacer, Input, Grid, Button } from "@nextui-org/react";
 
 export default function ApiKeys() {
     const router = useRouter();
-    const [data, setData] = useState(null);
-    const [msg, setMsg] = useState(null);
+    const [note, setNote] = useState("");
+    const [notificationMessage, setNotificationMessage] = useState("");
 
     useEffect(() => {
         checkUserLoggedIn()
             .then((data) => {
-                setData(data.user.first_name);
+                console.log(data)
             })
             .catch((error) => {
                 router.push("/login");
@@ -21,12 +21,55 @@ export default function ApiKeys() {
             });
     }, [router]);
 
+    const generateAPIKey = () => {
+        fetch("http://localhost:8000/api-keys", {
+            credentials: "include",
+            method: "POST"
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setNotificationMessage("API Keys Generated Successfully")
+            })
+            .catch((e) => {
+                console.log(e)
+                setNotificationMessage("Try again later!");
+            })
+    }
     return (
         <>
+            {!!notificationMessage && <Text>{notificationMessage}</Text>}
             <Container gap={2} justify="center">
-                <Text h1 align-items="center">
-                    API Keys
-                </Text>
+                <Row>
+                    <Text h1 align-items="center">
+                        API Keys
+                    </Text>
+                </Row>
+                <Row>
+                    <Text align-items="center">
+                        Manage on your API keys on this page
+                    </Text>
+                </Row>
+                <Spacer y={2} />
+                <Row>
+                    <Text align-items="center">
+                        What is this token for?
+                    </Text>
+                </Row>
+                <Spacer y={1} />
+                <Grid>
+                    <Input
+                        bordered
+                        size="lg"
+                        color="primary"
+                        labelPlaceholder="Note"
+                        value={note}
+                        onChange={(e) => setNote(e.target.value)}
+                        css={{ mb: "6px" }}
+                    />
+                </Grid>
+                <Spacer y={1} />
+                <Button onPress={generateAPIKey}>Generate</Button>
             </Container>
         </>
     );
