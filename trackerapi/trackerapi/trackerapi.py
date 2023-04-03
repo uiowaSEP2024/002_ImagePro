7addd3bdb3dc0ee6b41923adce13197fe7e90abe
bainwala <hbainwala@gmail.com>
2023-04-02 19:06:29 -0500

    Job and Event Classes

--- trackerapi/trackerapi/trackerapi.py
+++ trackerapi/trackerapi/trackerapi.py
@@ -32,3 +32,23 @@ class TrackerAPI:
         return self.__to_json(
             requests.patch(url, json=data, headers=self.__get_headers())
         )
+
+
+class Job:
+    def __init__(self, api: TrackerAPI, provider_job_id=None):
+        self.provider_job_id = provider_job_id
+        self.api = api
+
+    def send_event(self, kind=None, name=None):
+        return self.api.send_event(
+            kind=kind, name=name, provider_job_id=self.provider_job_id
+        )
+
+
+class Event:
+    def __init__(self, api: TrackerAPI, event_id=None):
+        self.event_id = event_id
+        self.api = api
+
+    def send_metadata(self, data=None):
+        self.api.send_event_metadata(event_id=self.event_id, metadata={})
