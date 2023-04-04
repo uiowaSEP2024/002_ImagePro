f11a13eb2f8d3fd8debae8ceb4ba4bb9ad5053d0
bainwala <hbainwala@gmail.com>
2023-04-03 23:42:19 -0500

    Updated mockscript to use functions from wrapper library instead of http requests

---
+++ mockjob/mockscript.py
@@ -0,0 +1,95 @@
+import json
+import os
+import random
+import sys
+import time
+import uuid
+from datetime import datetime
+from pathlib import Path
+
+import requests
+from dotenv import load_dotenv
+
+path_root = Path(__file__).parents[1]
+sys.path.append(f"{path_root}/trackerapi")
+
+from trackerapi.trackerapi import TrackerAPI
+
+load_dotenv()
+
+TEAM3_API_KEY = os.environ.get("TEAM3_API_KEY")
+
+LOG_FILE_PATH = "logs.txt"
+
+
+def generate_uuid():
+    # Create a random uuid
+    return str(uuid.uuid4())
+
+
+def printf(file, data):
+    # Write the data to a file
+    file.write(data)
+
+
+# Send request to create the JOB
+# Replace with send_event(....)
+
+
+def run_mock_job(customer_id=None):
+    steps = 20
+    job_id = generate_uuid()
+
+    # Create TrackerAPI object
+
+    tracker = TrackerAPI("q-jAqPWCRGr2u6SeK6r6U0LBfJA")
+
+    # Create a Job Object
+
+    job_tracker = tracker.create_job(job_id, customer_id, "MockscriptJob")
+
+    with open(LOG_FILE_PATH, "a+") as outfile:
+        # Do a dummy job for N steps
+        for i in range(steps):
+            # Do some work lasting anywhere between 1-2 seconds
+            time.sleep(random.randint(1, 2))
+
+            # Prepare log data
+            json_data = {
+                "job_id": job_id,
+                "customer_id": customer_id,
+                "step": "step {}".format(i),
+                "time": str(datetime.now()),
+            }
+
+            json_str = json.dumps(json_data)
+
+            # Print json to file
+            # printf(outfile, json_str + "\n")
+
+            # Convenience print to console for live log
+            # just so we don't have to sit and wait for file to be logged to
+            print(json_str)
+
+            is_last_step = i == steps - 1
+            if is_last_step:
+                kind = "complete"
+            else:
+                kind = "step"
+
+            job_tracker.send_event(kind, "step {}".format(i))
+
+
+if __name__ == "__main__":
+    sample_customer_ids = [
+        1,  # johndoe@gmail.com see backend/seed.py
+        2,  # janeblack@gmail.com see backend/seed.py
+    ]
+
+    # Select a random customer
+    random_customer_id = sample_customer_ids[
+        random.randrange(0, len(sample_customer_ids))
+    ]
+
+    # Run job for the chosen customer
+    run_mock_job(customer_id=1)
