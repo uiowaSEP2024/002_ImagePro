6d418c2de3e65da53455f9a2262da3e1842cf04f
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-04-11 12:11:00 -0500

    frontend tests

--- frontend/__tests__/signup.test.tsx
+++ frontend/__tests__/signup.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent, getByTestId } from "@testing-library/react";
 import Signup from "@/pages/signup";
 import "@testing-library/jest-dom";
 import { useRouter } from "next/router";
@@ -46,4 +46,29 @@ describe("Signup", () => {
     expect(text).toBeInTheDocument();
   
   });
+
+  it('signup on change', () => {
+    const sendSignUpReq = jest.fn((value) => {});
+    
+    const { queryByPlaceholderText } = render(<Signup />);
+
+    const firstNameInput = queryByPlaceholderText('First Name') as HTMLInputElement;
+    fireEvent.input(firstNameInput, { target: { value: 'John' } });
+    const lastNameInput = queryByPlaceholderText('Last Name') as HTMLInputElement;
+    fireEvent.input(lastNameInput, { target: { value: 'Smith' } });
+    const emailInput = queryByPlaceholderText('Email')  as HTMLInputElement;
+    fireEvent.input(emailInput, { target: { value: 'abc@gmail.com' } });
+    const passInput = queryByPlaceholderText('Password')  as HTMLInputElement;
+    fireEvent.input(passInput, { target: { value: 'abc' } });
+    const pass2Input = queryByPlaceholderText('Confirm Password')  as HTMLInputElement;
+    fireEvent.input(pass2Input, { target: { value: 'abc' } });
+
+    expect(firstNameInput.value).toBe('John');
+
+    const button =  screen.getByRole("button");
+    fireEvent.click(button);
+    
+    expect(sendSignUpReq).toHaveBeenCalled();
+  });
+
 });
