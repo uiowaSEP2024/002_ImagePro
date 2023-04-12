806e2fdc9f3fbd6440b6b224cba958fb3fdfe9cc
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-04-12 11:50:09 -0500

    Base provider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,16 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "src/components/Navbar";
 // 1. import `NextUIProvider` component
-import { NextUIProvider } from "@nextui-org/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <ChakraProvider>
       {/* Render the Navbar */}
       <Navbar />
       {/* Render the page */}
       <Component {...pageProps} />
-    </NextUIProvider>
+    </ChakraProvider>
   );
 }
 
