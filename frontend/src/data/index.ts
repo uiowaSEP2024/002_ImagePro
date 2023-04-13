3f09aacebbfb327026d166f83cdf630d3949d358
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-04-12 22:40:49 -0500

    refactor: move all fetch helpers into data/index.ts

--- frontend/src/data/index.ts
+++ frontend/src/data/index.ts
@@ -132,7 +132,7 @@ export const generateAPIKeys = async () => {
 }
 
 export const fetchAPIkeys = async (): Promise<Key[] | void> => {
-  return await fetch('http://localhost:8000/api-keys', {
+  return await fetch("http://localhost:8000/api-keys", {
     credentials: "include",
     method: "GET"
   }).
@@ -163,3 +163,77 @@ export const fetchEvents = async (
       console.log(e);
     });
 };
+
+
+export type User = {
+  first_name: string
+  last_name: string
+  email: string
+}
+
+export async function fetchCheckUserLoggedIn() {
+  try {
+    const result = await 
+    fetch("http://localhost:8000/login", {
+      credentials: "include",
+      method: "GET",
+    }) 
+      
+    return result.json() as unknown as {user: User, message: string} 
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+
+export const fetchLogout = async () => {
+  const response = await fetch("http://localhost:8000/logout", {
+    method: "POST",
+    credentials: "include",
+  })
+
+  return response.json()
+};
+
+
+export const fetchLogin = async (email: string, password: string) => {
+  const response = await fetch("http://localhost:8000/login", {
+    credentials: "include",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    body: new URLSearchParams({
+      username: email,
+      password: password,
+    }),
+  })
+
+  return await response.json() as {user: User}
+}
+
+
+
+type UserCreate = {
+  email: string
+  password: string
+  first_name: string
+  last_name: string
+}
+export const fetchSignUp = async (data: UserCreate) => {
+  const response = await fetch("http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: data.email,
+      password: data.password,
+      first_name: data.first_name,
+      last_name: data.last_name,
+    }),
+  })
+
+  return await response.json()
+}
