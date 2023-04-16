9086e68abdf3d2995ce4bfd504ded7c61d3d9ec3
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-04-16 11:41:11 -0500

    modify seed file

--- backend/seed.py
+++ backend/seed.py
@@ -59,30 +59,45 @@ EVENTS_DATA = [
         provider_job_id="botimage-123",
         kind="step",
         name="Scanning Right Kidney",
+        event_metadata={
+            "official": "Yes"
+        }
     ),
     #  Job 1, Event 3
     dict(
         provider_job_id="botimage-123",
         kind="complete",
         name="Analyze Kidney Results",
+        event_metadata={
+            "official": "Yes"
+        }
     ),
     #  Job 2, Event 1
     dict(
         provider_job_id="noodlesco-123",
         kind="step",
         name="Scanning Left Lung",
+        event_metadata={
+            "official": "Yes"
+        }
     ),
     #  Job 2, Event 2
     dict(
         provider_job_id="noodlesco-123",
         kind="step",
         name="Scanning Right Lung",
+        event_metadata={
+            "official": "Yes"
+        }
     ),
     #  Job 2, Event 3
     dict(
         provider_job_id="noodlesco-123",
         kind="complete",
         name="Analyze Lung Results",
+        event_metadata={
+            "official": "Yes"
+        }
     ),
 ]
 
