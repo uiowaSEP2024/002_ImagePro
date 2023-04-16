0e55250acdb36fd1043c6229fc2a85ef681b4297
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-04-15 23:05:21 -0500

    Sending metadata works

--- backend/alembic/versions/97c85d02b221_add_metadata_column.py
+++ backend/alembic/versions/97c85d02b221_add_metadata_column.py
@@ -1,8 +1,8 @@
 """add metadata_column
 
-Revision ID: 408b1c104fdf
+Revision ID: 97c85d02b221
 Revises: b32b411f19a2
-Create Date: 2023-04-15 11:46:25.793397
+Create Date: 2023-04-15 22:48:49.447890
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = '408b1c104fdf'
+revision = '97c85d02b221'
 down_revision = 'b32b411f19a2'
 branch_labels = None
 depends_on = None
