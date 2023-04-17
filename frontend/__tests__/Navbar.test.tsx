9a1faa46ad442a785eda55bd09a98dd1bcb3f67c
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-04-16 21:04:15 -0500

    chore: merge conflicts from main

--- frontend/__tests__/Navbar.test.tsx
+++ frontend/__tests__/Navbar.test.tsx
@@ -1,39 +1,28 @@
 // __tests__/navbar.test.jsx
 
-import { render, screen, RenderResult } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Navbar from "../src/components/Navbar";
 import "@testing-library/jest-dom";
-import { useRouter } from "next/router";
 
-jest.mock('next/router', () => ({
+jest.mock("next/router", () => ({
   useRouter() {
-    return ({
-      route: '/',
-      pathname: '',
-      query: '',
-      asPath: '',
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
       push: jest.fn(),
       events: {
         on: jest.fn(),
         off: jest.fn()
-      },  beforePopState: jest.fn(() => null),
+      },
+      beforePopState: jest.fn(() => null),
       prefetch: jest.fn(() => null)
-    });
-  },
-}));
-
-const router = useRouter()
-
-jest.mock('@/utils/auth', () => ({
-  checkUserLoggedIn() {
-    return new Promise((resolve) => {
-      resolve( {detail : "already logged in!"} )
-    });
-  },
+    };
+  }
 }));
 
 describe("NavBar", () => {
-
   it("renders navbar unchanged", () => {
     render(<Navbar />);
   });
