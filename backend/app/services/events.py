6adde13c92d96d6a0358fced39b5b0dcfcb25d99
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-04-19 11:55:47 -0500

    minor changes

--- backend/app/services/events.py
+++ backend/app/services/events.py
@@ -8,7 +8,7 @@ from .jobs import get_job_by_provider_job_id
 def create_event(db: Session, event: schemas.EventCreatePublic, provider):
     job = get_job_by_provider_job_id(db, event.provider_job_id, provider.id)
     # print(type(event.metadata))
-    db_event = models.Event(kind=event.kind, name=event.name, job_id=job.id, event_metadata=event.metadata)
+    db_event = models.Event(kind=event.kind, name=event.name, job_id=job.id, event_metadata=event.event_metadata)
     db.add(db_event)
     db.commit()
     db.refresh(db_event)
