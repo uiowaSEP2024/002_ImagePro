ade3f922482bde8fff40cdb7f575147b180bccf2
ryan-edwall <111799742+ryan-edwall@users.noreply.github.com>
2023-04-19 07:54:17 -0500

    ENH: Updated step configurations table

--- backend/app/models/step_configuration.py
+++ backend/app/models/step_configuration.py
@@ -1,28 +1,39 @@
 from sqlalchemy import Column, ForeignKey, UniqueConstraint
-from sqlalchemy.sql.sqltypes import String, Integer, Enum
+from sqlalchemy.sql.sqltypes import String, Integer
 from sqlalchemy.orm import relationship
 
 from .base import Base
 
 
-class EventConfiguration(Base):
-    __tablename__ = "event_configurations"
+class StepConfiguration(Base):
+    __tablename__ = "step_configurations"
     __table_args__ = (UniqueConstraint("tag"),)
 
     # Auto-generated internal job configuration id
     id = Column(Integer, primary_key=True, index=True)
 
-    # internal tag used to link a job
+    # internal tag used to link a job configuration
     job_tag = Column(String, index=True, nullable=False)
-
-    # internal tag used to link the configuration
+    #
+    # internal tag used to link a step configuration
     tag = Column(String, index=True, nullable=False)
+    #
+    # The step configuration name as specified by the provider
+    provider_step_configuration_name = Column(String, index=True, nullable=False)
 
-    # The event configuration name as specified by the provider
-    provider_event_configuration_name = Column(String, index=True, nullable=False)
-
-    # kind of event the configuration will be created for
-    kind = Column(
-        Enum("step", "error", "info", "complete", name="event_configuration_kind"),
-        nullable=False,
+    provider_job_configuration_id = Column(
+        Integer, ForeignKey("job_configurations.id"), index=True, nullable=False
     )
+    provider_job_configuration = relationship(
+        "JobConfiguration",
+        back_populates="provider_step_configurations",
+        foreign_keys=[provider_job_configuration_id],
+    )
+
+    # provider_metadata_configurations = relationship(
+    #     "MetadataConfiguration",
+    #     back_populates="provider_step_configuration",
+    # )
+
+    # TODO: This should be linked to billing configuration in the future
+    points = Column(Integer, index=True, nullable=False)
