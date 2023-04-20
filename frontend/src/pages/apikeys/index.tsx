18567fcb22ff70f1d1e57730186bef5f794c20c8
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-04-19 23:46:14 -0500

    TItle

--- frontend/src/pages/apikeys/index.tsx
+++ frontend/src/pages/apikeys/index.tsx
@@ -2,8 +2,7 @@ import { fetchAPIkeys } from "@/data";
 import { Key } from "@/data/types";
 import React from "react";
 import { useState, useEffect } from "react";
-import { Text } from "@nextui-org/react";
-import { Container, Row, Spacer, Input, Grid, Button, Card } from "@nextui-org/react";
+import { Text } from '@chakra-ui/react'
 import { useAuthContext, useEnsureAuthenticated } from "@/hooks/useAuthContext";
 
 export default function ApiKeys() {
@@ -47,53 +46,8 @@ export default function ApiKeys() {
       })
   }
   return (
-    <>
-      {!!notificationMessage && <Text>{notificationMessage}</Text>}
-      <Container gap={2} justify="center">
-        <Row>
-          <Text h1 align-items="center">
-                        API Keys
-          </Text>
-        </Row>
-        <Row>
-          <Text align-items="center">
-                        Manage your API keys on this page
-          </Text>
-        </Row>
-        <Spacer y={1} />
-        <Grid aria-label="Keys">
-          {keys.map((card) => (
-            <Grid key={card.id} data-testid="testkeys">
-              <Card>
-                <Card.Body aria-label="key">
-                  <Text>{card.key}</Text>
-                </Card.Body>
-              </Card>
-              <Spacer y={1} />
-            </Grid>
-          ))}
-        </Grid>
-        <Spacer y={2} />
-        <Row>
-          <Text align-items="center">
-                        What is this token for?
-          </Text>
-        </Row>
-        <Spacer y={1} />
-        <Grid>
-          <Input
-            bordered
-            size="lg"
-            color="primary"
-            labelPlaceholder="Note"
-            value={note}
-            onChange={(e) => setNote(e.target.value)}
-            css={{ mb: "6px" }}
-          />
-        </Grid>
-        <Spacer y={1} />
-        <Button onPress={generateAPIKey}>Generate</Button>
-      </Container>
-    </>
+    <Text fontSize='45px'>
+        Generate API Keys
+    </Text>
   );
 }
