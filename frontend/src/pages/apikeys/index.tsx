69e3761d550adf41d6dc54392ab58017cde59b5b
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-04-20 01:27:20 -0500

    page modified

--- frontend/src/pages/apikeys/index.tsx
+++ frontend/src/pages/apikeys/index.tsx
@@ -2,52 +2,84 @@ import { fetchAPIkeys } from "@/data";
 import { Key } from "@/data/types";
 import React from "react";
 import { useState, useEffect } from "react";
-import { Text } from '@chakra-ui/react'
+import { Heading, Text, Grid, GridItem, Card, CardBody, Spacer, Input, Button } from '@chakra-ui/react'
 import { useAuthContext, useEnsureAuthenticated } from "@/hooks/useAuthContext";
 
 export default function ApiKeys() {
-  const {currentUser} = useAuthContext()
-  const [note, setNote] = useState("");
-  const [notificationMessage, setNotificationMessage] = useState("");
-  const [keys, setKeys] = useState<Key[]>([]);
+    const { currentUser } = useAuthContext()
+    const [note, setNote] = useState("");
+    const [notificationMessage, setNotificationMessage] = useState("");
+    const [keys, setKeys] = useState<Key[]>([]);
 
-  useEnsureAuthenticated()
+    useEnsureAuthenticated()
 
 
-  useEffect(() => {
+    useEffect(() => {
 
-    async function loadKeys() {
-      const data = await fetchAPIkeys();
-      if (data) setKeys(data);
+        async function loadKeys() {
+            const data = await fetchAPIkeys();
+            if (data) setKeys(data);
+        }
+
+        console.log({ currentUser })
+
+        // if (currentUser){
+        loadKeys() // TODO: wrap this back in check for currentUser
+        // }
+
+    }, [currentUser]);
+
+
+
+    const generateAPIKey = () => {
+        fetch("http://localhost:8000/api-keys", {
+            credentials: "include",
+            method: "POST"
+        })
+            .then((response) => response.json())
+            .then(() => {
+                setNotificationMessage("API Keys Generated Successfully")
+            })
+            .catch((e) => {
+                console.log(e)
+                setNotificationMessage("Try again later!");
+            })
     }
 
-    console.log({currentUser})
 
-    // if (currentUser){
-    loadKeys() // TODO: wrap this back in check for currentUser
-    // }
-
-  }, [currentUser]);
-
-
-
-  const generateAPIKey = () => {
-    fetch("http://localhost:8000/api-keys", {
-      credentials: "include",
-      method: "POST"
-    })
-      .then((response) => response.json())
-      .then(() => {
-        setNotificationMessage("API Keys Generated Successfully")
-      })
-      .catch((e) => {
-        console.log(e)
-        setNotificationMessage("Try again later!");
-      })
-  }
-  return (
-    <Text fontSize='45px'>
-        Generate API Keys
-    </Text>
-  );
+    return (
+        <Grid px='50' py='50'>
+            <GridItem>
+                <Heading>
+                    API Keys
+                </Heading>
+            </GridItem>
+            <GridItem>
+                <Text>
+                    Manage API Keys for yout provider account
+                </Text>
+            </GridItem>
+            <Spacer height="30px" />
+            <GridItem>
+                {keys.map((card) => (
+                    <Grid key={card.id} data-testid="testkeys">
+                        <Card>
+                            <CardBody aria-label="key">
+                                <Text>{card.key}</Text>
+                            </CardBody>
+                        </Card>
+                        <Spacer height="30px" />
+                    </Grid>
+                ))}
+            </GridItem>
+            <Spacer height="50px" />
+            <GridItem>
+                <Input placeholder="Note" size='lg' width='300px' />
+            </GridItem>
+            <Spacer height="30px" />
+            <GridItem>
+                <Button colorScheme='telegram'>Create Key</Button>
+            </GridItem>
+        </Grid>
+    );
 }
