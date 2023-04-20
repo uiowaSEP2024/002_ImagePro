a50408e39e26a2f3d1b85901d75eddb52e35c3e8
ryan-edwall <111799742+ryan-edwall@users.noreply.github.com>
2023-04-19 20:16:18 -0500

    ENH: Created test for job configuration model

--- backend/app/models/step_configuration.py
+++ backend/app/models/step_configuration.py
@@ -14,20 +14,19 @@ class StepConfiguration(Base):
 
     # internal tag used to link a job configuration
     job_tag = Column(String, index=True, nullable=False)
-    #
     # internal tag used to link a step configuration
     tag = Column(String, index=True, nullable=False)
     #
     # The step configuration name as specified by the provider
     provider_step_configuration_name = Column(String, index=True, nullable=False)
 
-    provider_job_configuration_id = Column(
+    job_configuration_id = Column(
         Integer, ForeignKey("job_configurations.id"), index=True, nullable=False
     )
     provider_job_configuration = relationship(
         "JobConfiguration",
         back_populates="provider_step_configurations",
-        foreign_keys=[provider_job_configuration_id],
+        foreign_keys=[job_configuration_id],
     )
 
     provider_metadata_configurations = relationship(
