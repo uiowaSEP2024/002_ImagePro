38244805c7567c536e2757e05b1490cda45dd6da
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-04-21 14:55:27 -0500

    Alert Dialog popping

--- frontend/src/pages/apikeys/index.tsx
+++ frontend/src/pages/apikeys/index.tsx
@@ -1,15 +1,36 @@
 import { fetchAPIkeys } from "@/data";
 import { Key } from "@/data/types";
-import React from "react";
+import React, { useRef } from "react";
 import { useState, useEffect } from "react";
-import { Heading, Text, Grid, GridItem, Card, CardBody, Spacer, Input, Button, Container } from "@chakra-ui/react"
+import {
+    Heading,
+    Text,
+    Grid,
+    GridItem,
+    Card,
+    CardBody,
+    Spacer,
+    Input,
+    Button,
+    Container,
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    useDisclosure,
+} from "@chakra-ui/react"
+import { FocusableElement } from '@chakra-ui/utils'
 import { useAuthContext, useEnsureAuthenticated } from "@/hooks/useAuthContext";
 
 export default function ApiKeys() {
     const { currentUser } = useAuthContext()
     const [note, setNote] = useState("");
-    const [notificationMessage, setNotificationMessage] = useState("");
     const [keys, setKeys] = useState<Key[]>([]);
+    const [firstKeys, setFirstKeys] = useState<Key[]>([])
+    const [isOpen, setIsOpen] = useState(false);
+    const cancelRef = React.useRef(null)
 
     useEnsureAuthenticated()
 
@@ -45,12 +66,11 @@ export default function ApiKeys() {
         })
             .then((response) => response.json())
             .then((data) => {
-                console.log(data)
-                setNotificationMessage("API Keys Generated Successfully")
+                if (data) setFirstKeys(data)
+                setIsOpen(true)
             })
             .catch((e) => {
                 console.log(e)
-                setNotificationMessage("Try again later!");
             })
     }
 
@@ -65,7 +85,7 @@ export default function ApiKeys() {
                 </GridItem>
                 <GridItem>
                     <Text>
-                        Manage API Keys for yout provider account
+                        Manage API Keys for your provider account
                     </Text>
                 </GridItem>
                 <Spacer height="30px" />
@@ -91,7 +111,32 @@ export default function ApiKeys() {
                     <Button colorScheme='telegram' onClick={generateAPIKey}>Create Key</Button>
                 </GridItem>
             </Grid>
-        </Container>
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={() => {
+                    setIsOpen(false);
+                }}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                            API Key 
+                        </AlertDialogHeader>
 
+                        <AlertDialogBody>
+                            Please copy this key for later. This is the only time you will see it.
+                        </AlertDialogBody>
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={() => {
+                                setIsOpen(false);
+                            }}>
+                                Ok
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </Container>
     );
 }
