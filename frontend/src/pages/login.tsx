c4484c27566788ad052add490fd7c4fc43ce4029
bainwala <hbainwala@gmail.com>
2023-04-21 12:21:44 -0500

    Added container and form

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { Flex, Text, Box, Input, Button, InputGroup, InputRightElement, Link } from "@chakra-ui/react";
+import { Container, Text, Box, Input, Button, InputGroup, InputRightElement, Link, Center, VStack } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons"
 import { useAuthContext } from "@/hooks/useAuthContext";
 import { withUnauthenticated } from "@/components/withAuthenticated";
@@ -16,7 +16,9 @@ function Login() {
 
   const { logIn } = useAuthContext();
 
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    console.log("Submit")
     try {
       const result = await logIn(email, password);
       if (result && result.user) {
@@ -30,31 +32,37 @@ function Login() {
   };
 
   return (
-    <Flex direction='column' padding='30px'>
-      {!!notificationMessage && <Text>{notificationMessage}</Text>}
-      <Text role="heading" fontSize='36px' as='b' marginBottom='10px'>Login</Text>
-      <Box flex='1' w='200px' bg='white' marginBlock='5px'>
-        <Text fontFamily='20px' fontWeight='500'>Email</Text>
-        <Input variant='filled' size='sm' placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-      </Box>
-      <Box flex='1' w='200px' bg='white' marginBlock='5px'>
-        <Text fontFamily='20px' fontWeight='500'>Password</Text>
-        <InputGroup>
-          <Input variant='filled' size='sm' placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} type={showPassword ? "text" : "password"}/>
-          <InputRightElement width='4.0rem' height='2.0rem'>
-            <Button h='1.5rem' size='xs' onClick={() => setShowPassword(!showPassword)}>
-              {showPassword ? "Hide" : "Show"}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </Box>
-      <Box flex='1' w='100px' marginBlock='10px'>
-        <Button size='sm' name="login" role="button" data-testid="login" onClick={handleLogin}>Login</Button>
-      </Box>
-      <Link w='fit-content' href="/signup">
+    <Container display={"flex"} justifyContent={"center"} maxW={"container.lg"} py={"6"}>
+      <Center maxW={"xs"} display="flex" flexDirection='column' paddingTop={"20vh"}>
+        {!!notificationMessage && <Text>{notificationMessage}</Text>}
+        <form onSubmit={handleLogin}> 
+          <VStack spacing={4}>
+            <Text role="heading" fontSize='36px' as='b' marginBottom='10px'>Login</Text>
+            <Box w={"100%"} flex={1} bg='white' marginBlock='5px'>
+              <Text fontFamily='20px' fontWeight='500'>Email</Text>
+              <Input variant='filled' placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </Box>
+            <Box flex={1}  w={"100%"}  bg='white' marginBlock='5px'>
+              <Text fontFamily='20px' fontWeight='500'>Password</Text>
+              <InputGroup>
+                <Input variant='filled' placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} type={showPassword ? "text" : "password"}/>
+                <InputRightElement w={"max-content"} height='100%'>
+                  <Button h='100%' px={1} size='xs' onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? "Hide" : "Show"}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </Box>
+        
+            <Button type="submit" alignSelf={"flex-start"} size='sm' name="login" role="button" data-testid="login" >Login</Button>
+        
+            <Link w='fit-content' href="/signup">
         New user? Create an account. <ExternalLinkIcon mx='2px' mb='2px' />
-      </Link>
-    </Flex>
+            </Link>
+          </VStack>
+        </form>
+      </Center>
+    </Container>
   );
 }
 
