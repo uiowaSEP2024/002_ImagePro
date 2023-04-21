2c6de3310f4f1a85cf13ae09949e32287c6e58c6
bainwala <hbainwala@gmail.com>
2023-04-20 20:31:04 -0500

    Modified Login page to use chakra UI

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import {
-  Card,
-  Spacer,
-  Button,
-  Text,
-  Input,
-  Link,
-  Container,
-  Row
-} from "@nextui-org/react";
+import { Flex, Text, Box, Input, Button, InputGroup, InputRightElement, Link } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons"
 import { useAuthContext } from "@/hooks/useAuthContext";
 import { withUnauthenticated } from "@/components/withAuthenticated";
 
 function Login() {
   const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false)
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -37,56 +30,31 @@ function Login() {
   };
 
   return (
-    <div>
+    <Flex direction='column' padding='30px'>
       {!!notificationMessage && <Text>{notificationMessage}</Text>}
-      <Container
-        display="flex"
-        alignItems="center"
-        justify="center"
-        css={{ minHeight: "100vh" }}
-      >
-        <Card css={{ mw: "420px", p: "20px" }} variant="bordered">
-          <Text
-            size={24}
-            weight="bold"
-            h1
-            align-items="center"
-            css={{
-              as: "center",
-              mb: "20px"
-            }}
-          >
-            Login
-          </Text>
-          <Input
-            color="primary"
-            size="lg"
-            placeholder="Email"
-            aria-label="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Spacer y={1} />
-          <Input
-            color="primary"
-            size="lg"
-            placeholder="Password"
-            aria-label="Password"
-            type={"password"}
-            css={{ mb: "6px" }}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Row justify="space-between">
-            <Link block color="secondary" href="/signup">
-              New user? Create Account.{" "}
-            </Link>
-          </Row>
-          <Spacer y={1} />
-          <Button data-testid="login" name="login" onPress={handleLogin}>Log in</Button>
-        </Card>
-      </Container>
-    </div>
+      <Text fontSize='36px' as='b' marginBottom='10px'>Login</Text>
+      <Box flex='1' w='200px' bg='white' marginBlock='5px'>
+        <Text fontFamily='20px' fontWeight='500'>Email</Text>
+        <Input variant='filled' size='sm' placeholder="First Name" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      </Box>
+      <Box flex='1' w='200px' bg='white' marginBlock='5px'>
+        <Text fontFamily='20px' fontWeight='500'>Password</Text>
+        <InputGroup>
+          <Input variant='filled' size='sm' placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} type={showPassword ? "text" : "password"}/>
+          <InputRightElement width='4.0rem' height='2.0rem'>
+            <Button h='1.5rem' size='xs' onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </Box>
+      <Box flex='1' w='100px' marginBlock='10px'>
+        <Button size='sm' name="signup" role="button" data-testid="signup" onClick={handleLogin}>Login</Button>
+      </Box>
+      <Link w='fit-content' href="/signup">
+        New user? Create an account. <ExternalLinkIcon mx='2px' mb='2px' />
+      </Link>
+    </Flex>
   );
 }
 
