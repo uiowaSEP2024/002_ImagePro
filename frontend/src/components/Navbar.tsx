8161528a9fab1f2cbc1d174ad041bf69144658b4
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-04-23 16:23:08 -0500

    Adding navbar Chakra

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,50 +1,98 @@
 import { useAuthContext } from "@/hooks/useAuthContext";
-import { Navbar, Button } from "@nextui-org/react";
+import { ReactNode } from 'react';
+import { Button } from "@nextui-org/react";
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  useDisclosure,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import React from "react";
 
 
 const TopNavbar = () => {
   const { currentUser, logOut} = useAuthContext()
 
+  const NavLink = ({ children, link }: { children: ReactNode, link: string }) => (
+    <Link
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+      href={link}>
+      {children}
+    </Link>
+  )
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   if (!currentUser) {
     return (
-      <Navbar variant="sticky">
-        <Navbar.Content
-          activeColor="primary"
-          enableCursorHighlight
-          hideIn="xs"
-          variant="underline"
-        >
-          <Navbar.Link href="/">Home</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content enableCursorHighlight hideIn="xs">
-          <Navbar.Link data-testid="loginButton" href="/login">Login</Navbar.Link>
-          <Navbar.Link href="/signup">Sign up</Navbar.Link>
-        </Navbar.Content>
-      </Navbar>
-    )}
+      <>
+        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            <HStack spacing={8} alignItems={'center'}>
+              <HStack
+                as={'nav'}
+                spacing={4}
+                display={{ base: 'none', md: 'flex' }}>
+                  <NavLink link={'/'}>Home</NavLink>
+              </HStack>
+            </HStack>
 
+          <Flex alignItems={'center'}>
+            <HStack spacing={8} alignItems={'center'}>
+              <HStack
+                as={'nav'}
+                spacing={4}
+                display={{ base: 'none', md: 'flex' }}>
+                  <NavLink data-testid="loginButton" link={'/login'}>Login</NavLink>
+                  <NavLink link={'/signup'}>Sign up</NavLink>
+              </HStack>
+            </HStack>
+          </Flex>
+          </Flex>
+        </Box>
+      </>
+    );
+  }
   return (
-    <Navbar variant="sticky">
-      <Navbar.Content
-        activeColor="primary"
-        enableCursorHighlight
-        hideIn="xs"
-        variant="underline"
-      >
-        <Navbar.Link href="/">Home</Navbar.Link>
-        <Navbar.Link href="/dashboard">Dashboard</Navbar.Link>
-        <Navbar.Link href="/billing">Billing</Navbar.Link>
-        <Navbar.Link href="/apikeys">Generate API Keys</Navbar.Link>
-      </Navbar.Content>
-      <Navbar.Content enableCursorHighlight hideIn="xs">
-        <Navbar.Link href="/profile">My Profile</Navbar.Link>
-        <Button flat light color="default" data-testid="logoutButton" name="logoutButton" onPress={logOut}>
-            Log Out
-        </Button>
-      </Navbar.Content>
-    </Navbar>
-  )
-}
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <HStack spacing={8} alignItems={'center'}>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+                <NavLink link={'/'}>Home</NavLink>
+                <NavLink link={'/dashboard'}>Dashboard</NavLink>
+                <NavLink link={'/billing'}>Billing</NavLink>
+                <NavLink link={'/apikeys'}>Generate API Keys</NavLink>
+            </HStack>
+          </HStack>
+
+        <Flex alignItems={'center'}>
+          <HStack spacing={8} alignItems={'center'}>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+                <NavLink link={'/profile'}>My Profile</NavLink>
+                <Button  data-testid="logoutButton" onPress={logOut}>Logout</Button>
+            </HStack>
+          </HStack>
+        </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+    
+  }
 
 export default TopNavbar;
