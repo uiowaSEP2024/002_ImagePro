35fa40728ee04e1a93296ff673d91c50360062ce
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-04-22 22:53:29 -0500

    refactor: update trackerapi api

---
+++ trackerapi/trackerapi/schemas.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict
+
+from pydantic import BaseModel, StrictInt, StrictStr
+
+
+class StepConfig(BaseModel):
+    tag: StrictStr
+    points: StrictInt
+    name: StrictStr
+
+    def __init__(self, name: str, tag: str, points: int, **kwargs):
+        super().__init__(name=name, tag=tag, points=points, **kwargs)
+
+
+class JobConfig(BaseModel):
+    tag: StrictStr
+    name: StrictStr
+    steps: List[StepConfig]
+
+    def __init__(self, name: str, tag: str, steps: List[StepConfig], **kwargs):
+        super().__init__(name=name, tag=tag, steps=steps, **kwargs)
+
+
+class JobConfigBulk(BaseModel):
+    configs: List[JobConfig]
