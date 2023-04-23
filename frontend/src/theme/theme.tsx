57708a40479fc6bf3b486c26bc41ff1a95c0af9f
bainwala <hbainwala@gmail.com>
2023-04-22 20:16:51 -0500

    Matched theme with login branch theme

--- frontend/src/theme/theme.tsx
+++ frontend/src/theme/theme.tsx
@@ -1,12 +1,31 @@
 import { extendTheme, ThemeConfig, ThemeOverride } from "@chakra-ui/react";
+import { DM_Sans } from "@next/font/google";
 
 export const themeConfig: ThemeConfig = {
   initialColorMode: "light",
   useSystemColorMode: false
 };
 
+const breakpoints = {
+  sm: "30em",
+  md: "48em",
+  lg: "62em",
+  xl: "80em",
+  "2xl": "96em"
+};
+
+export const fontPrimary = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"]
+});
+
 const themeOverride: ThemeOverride = {
   config: themeConfig,
+  fonts: {
+    body: fontPrimary.style.fontFamily,
+    heading: fontPrimary.style.fontFamily
+  },
+  breakpoints,
   components: {
     Button: {
       baseStyle: {
@@ -25,15 +44,14 @@ const themeOverride: ThemeOverride = {
           bg: "#2600BF",
           boxShadow: "0 0 2px 2px #efdfde"
         },
-        sm: {
+        regular: {
           bg: "#2600BF",
           fontSize: "md"
         }
       },
       defaultProps: {
         size: "lg",
-        variant: "sm",
-        colorScheme: "#2600BF"
+        variant: "regular",
       }
     }
   }
