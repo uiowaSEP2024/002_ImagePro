e7087d521a5e296add4426d7966920782a569aa5
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-04-28 19:15:20 -0500

    fixing linting

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -3,7 +3,6 @@ import { withAuthenticated } from "@/components/withAuthenticated";
 import {
   Box,
   VStack,
-  Button,
   Flex,
   Divider,
   chakra,
@@ -12,11 +11,10 @@ import {
   Container,
   Heading,
   Link,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import { fetchJobs } from "@/data";
 import { Job } from "@/data/types";
 import { useState, useEffect } from "react";
-import { Chakra_Petch } from "@next/font/google";
 
 interface FeatureProps {
   heading: string;
@@ -54,22 +52,22 @@ function Profile() {
     <Box as={Container} maxW="7xl" mt={14} p={4}>
       <Grid
         templateColumns={{
-          base: 'repeat(1, 1fr)',
-          sm: 'repeat(2, 1fr)',
-          md: 'repeat(2, 1fr)',
+          base: "repeat(1, 1fr)",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(2, 1fr)",
         }}
         gap={4}>
         <GridItem colSpan={1}>
           <Box p="50" w="100%" height="100%" borderWidth='1px' borderRadius='lg' overflow='hidden'>
             <VStack alignItems="flex-start" spacing="20px">
               <chakra.h1 fontSize="10xl" fontWeight="700" color="black">
-              {currentUser?.first_name} {currentUser?.last_name}
+                {currentUser?.first_name} {currentUser?.last_name}
               </chakra.h1>
               <chakra.h2 fontSize="3xl" fontWeight="700" color="blue.400">
-              {currentUser?.email}
+                {currentUser?.email}
               </chakra.h2>
               <chakra.h2 data-testid="role" fontSize="3xl" fontWeight="700" color="red.400">
-              {currentUser?.role}
+                {currentUser?.role}
               </chakra.h2>
             </VStack>
           </Box>
@@ -80,7 +78,7 @@ function Profile() {
               <VStack alignItems="center" spacing="10px">
                 <chakra.h3 fontSize="1xl">
                   At BotImage, we strive to provide a fully extensible and scaleable solution to even the most complex jobs.
-                  As a valued customer, you've used our site to track 
+                  As a valued customer, you have used our site to track 
                 </chakra.h3>
                 <chakra.h1 alignItems="centered" p="10" fontSize="5xl" fontWeight="700" fontStyle="bold" color="purple.500">
                   {jobs.length} jobs
@@ -91,20 +89,20 @@ function Profile() {
         </GridItem>
       </Grid>
       <Divider mt={12} mb={12} />
-      <Heading fontSize={'3xl'} p="5">Recent Jobs</Heading>
+      <Heading fontSize={"3xl"} p="5">Recent Jobs</Heading>
       <Grid
         templateColumns={{
-          base: 'repeat(1, 1fr)',
-          sm: 'repeat(2, 1fr)',
-          md: 'repeat(4, 1fr)',
+          base: "repeat(1, 1fr)",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(4, 1fr)",
         }}
-        gap={{ base: '8', sm: '12', md: '16' }}>
-          {jobs.slice(0,4).map(job => (
-            <Box key={job.id} p="10" w="100%" height="100%" borderWidth='1px' borderRadius='lg' overflow='hidden'>
+        gap={{ base: "8", sm: "12", md: "16" }}>
+        {jobs.slice(0,4).map(job => (
+          <Box key={job.id} p="10" w="100%" height="100%" borderWidth='1px' borderRadius='lg' overflow='hidden'>
             <chakra.h3>{job.provider_job_name}</chakra.h3>
             <chakra.p>See this job <Link href='/jobs' color='blue.500'> here </Link> </chakra.p>
           </Box>
-          ))}
+        ))}
       </Grid>
     </Box>
   );
