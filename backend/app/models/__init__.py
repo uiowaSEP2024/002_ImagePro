d20954afa5a4afed86c13f4bc23fc9eae0112b7d
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-04-29 19:57:22 -0500

    feat: create step_configurations table plus fix tests

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,6 +4,6 @@ from .api_key import Apikey
 from .job import Job
 from .event import Event
 from .job_configuration import JobConfiguration
+from .step_configuration import StepConfiguration
 
-# from .step_configuration import StepConfiguration
 # from .metadata_configuration import MetadataConfiguration
