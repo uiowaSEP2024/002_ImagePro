0f3aab25b8431f1f2e4fa147e08c1bf9d6ea9bfb
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-04-30 00:34:52 -0500

    test: add test for creating configuration with updated version

--- backend/tests/routers/test_job_configurations.py
+++ backend/tests/routers/test_job_configurations.py
@@ -26,6 +26,42 @@ def test_create_job_configurations(app_client, random_provider_user_with_api_key
     assert len(response.json()["step_configurations"]) == 0
 
 
+def test_create_job_configurations_with_new_version(
+    db, app_client, random_provider_user_with_api_key
+):
+    services.create_job_configuration(
+        db,
+        provider_id=random_provider_user_with_api_key.id,
+        job_configuration=schemas.JobConfigurationCreate(
+            tag="lung_cancer",
+            name="Lung Cancer",
+            version="1.0.0",
+            step_configurations=[],
+        ),
+    )
+
+    data = {
+        "tag": "lung_cancer",
+        "name": "Lung Cancer Again",
+        "version": "1.0.1",
+        "step_configurations": [],
+    }
+
+    response = app_client.post(
+        "/job_configurations",
+        json=data,
+        headers={
+            API_KEY_HEADER_NAME: random_provider_user_with_api_key.api_keys[0].key
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.json()["name"] == "Lung Cancer Again"
+    assert response.json()["tag"] == "lung_cancer"
+    assert response.json()["version"] == "1.0.1"
+    assert len(response.json()["step_configurations"]) == 0
+
+
 def test_create_job_configuration_with_conflicting_version(
     db, app_client, random_provider_user_with_api_key
 ):
@@ -56,7 +92,6 @@ def test_create_job_configuration_with_conflicting_version(
     )
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    print(response.json())
 
 
 def test_create_job_configuration_with_same_information(
