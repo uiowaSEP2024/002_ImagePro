a6e5a8db57cf2b87efe0444e048e15067edef4fd
Anand Gopalakrishnan <anand-gopalakrishnan@uiowa.edu>
2023-04-30 23:21:35 -0500

    job configurations, new type

---
+++ frontend/src/components/stackedChart.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: "Jobs Insights",
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+
+
+export const data = {
+    labels,
+    datasets: [
+      
+    ],
+  };
+
+
