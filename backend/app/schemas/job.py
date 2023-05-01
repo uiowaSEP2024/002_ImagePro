a5d3e879c920d3d38acd1ac3c50fb24f4c5ce54e
bainwala <hbainwala@gmail.com>
2023-05-01 00:56:05 -0500

    Expose Parent Entities On Schema

--- backend/app/schemas/job.py
+++ backend/app/schemas/job.py
@@ -2,6 +2,9 @@ from datetime import datetime
 
 from pydantic import BaseModel, StrictStr
 
+from .job_configuration import JobConfiguration
+from .user import User
+
 
 class JobBase(BaseModel):
     provider_job_id: str
@@ -16,7 +19,10 @@ class Job(JobBase):
     id: int
     provider_id: int
     job_configuration_id: int
-    
+
+    provider: User
+    job_configuration: JobConfiguration
+
     created_at: datetime = None
     updated_at: datetime = None
 
