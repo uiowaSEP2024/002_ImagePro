418eeeeec6e54106fec3519a071196f35a4ed3b0
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-05-03 23:36:38 -0500

    feat: implement api_keys expiration backend

--- backend/app/services/api_keys.py
+++ backend/app/services/api_keys.py
@@ -1,8 +1,14 @@
+from fastapi import HTTPException
+from sqlalchemy import asc
+from starlette import status
+
 from app import models, schemas
 from app.internal import generate_apikey
 from passlib.context import CryptContext
 from pydantic import validate_arguments
 from sqlalchemy.orm import Session
+
+from datetime import datetime
 
 from .users import get_user
 
@@ -10,7 +16,46 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_api_keys_for_user(db: Session, user_id: int):
-    return get_user(db, user_id).api_keys
+    return (
+        db.query(models.Apikey)
+        .order_by(asc(models.Apikey.created_at))
+        .filter(models.Apikey.user_id == user_id)
+        .all()
+    )
+
+
+def get_apikey(db: Session, apikey_id: int) -> models.Apikey:
+    return db.query(models.Apikey).get(apikey_id)
+
+
+def is_apikey_expired(api_key: models.Apikey):
+    return (
+        isinstance(api_key.expires_at, datetime)
+        and api_key.expires_at.timestamp() <= datetime.now().timestamp()
+    )
+
+
+def expire_apikey_for_user(db: Session, user_id: int, id: int):
+    api_key = get_apikey(db, id)
+
+    if api_key.user_id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail={"msg": "Not Allowed"},
+        )
+
+    if is_apikey_expired(api_key):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"msg": "Cannot expire already expired key"},
+        )
+
+    api_key.expires_at = datetime.now()
+    db.add(api_key)
+    db.commit()
+    db.refresh(api_key)
+
+    return api_key
 
 
 @validate_arguments(config=dict(arbitrary_types_allowed=True))
