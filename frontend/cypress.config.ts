c6e6a6b664dd872ac4ef96e20af892e6ea572a96
tellegen@uiowa.edu <tellegen@uiowa.edu>
2023-05-05 02:28:51 -0500

    more cucumber with cypress. Webpack loader issue

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,52 @@
+import webpack from "@cypress/webpack-preprocessor";
+import { defineConfig } from "cypress";
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+
+  on(
+    "file:preprocessor",
+    webpack({
+      webpackOptions: {
+        resolve: {
+          extensions: [".ts", ".js"],
+        },
+        module: {
+          rules: [
+            {
+              test: /\.ts$/,
+              exclude: [/node_modules/],
+              use: [
+                {
+                  loader: "ts-loader",
+                },
+              ],
+            },
+            {
+              test: /\.feature$/,
+              use: [
+                {
+                  loader: "webpack",
+                  options: config,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+};
+
+export default {
+  e2e: {
+    setupNodeEvents,
+    baseUrl: 'http://localhost:3000',
+    specPattern: "__tests__/features/*.feature"
+  },
+};
