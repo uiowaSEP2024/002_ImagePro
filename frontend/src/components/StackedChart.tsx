e1b563b1a0653f223389d5dfa125be30e5623da6
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-05-07 21:36:04 -0500

    feat: update chart to show by year

--- frontend/src/components/StackedChart.tsx
+++ frontend/src/components/StackedChart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -7,9 +7,9 @@ import {
   Title,
   Tooltip,
   Legend,
+  ChartOptions
 } from "chart.js";
-import { useState } from "react";
-import { fetchJobs } from "@/data";
+
 import { Job } from "@/data/types";
 import { Bar } from "react-chartjs-2";
 
@@ -22,62 +22,79 @@ ChartJS.register(
   Legend
 );
 
-const colorsList = [
+const COLORS_LIST = [
   "rgb(255, 99, 132)",
   "rgb(75, 192, 192)",
   "rgb(53, 162, 235)",
   "rgb(89, 23, 235)",
-  "rgb(125, 69, 200)",
+  "rgb(125, 69, 200)"
 ];
 
-const JobsChart = () => {
-  const [jobs, setJobs] = useState<Job[]>([]);
-  const [counts, setCounts] = useState<Record<string, number[]>>({});
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 
-  useEffect(() => {
-    const getJobs = async () => {
-      const data = await fetchJobs();
-      if (data) {
-        setJobs(data);
-        const _counts: Record<string, number[]> = {};
+type JobsChartProps = {
+  year?: number;
+  jobs?: Job[];
+};
 
-        data.forEach(({ job_configuration, created_at }) => {
-          const name = job_configuration.name || "Other";
-          const month = new Date(created_at!).getMonth();
+const JobsChart: React.FC<JobsChartProps> = ({
+  year = new Date().getFullYear(),
+  jobs = []
+}) => {
+  const jobsForYear = useMemo(() => {
+    return jobs.filter((job) => {
+      return new Date(job.created_at!).getFullYear() === Number(year);
+    });
+  }, [jobs, year]);
 
-          if (!_counts[name]) {
-            _counts[name] = Array(12).fill(0);
-          }
-          _counts[name][month]++;
-        });
+  const counts = useMemo(() => {
+    return jobsForYear.reduce((acc, curr) => {
+      const { job_configuration, created_at } = curr;
+      const name = job_configuration.name || "Other";
+      const month = new Date(created_at!).getMonth();
 
-        // Set counts to use later
-        setCounts(_counts);
+      if (!acc[name]) {
+        acc[name] = Array(12).fill(0);
       }
-    };
-    getJobs();
-  }, []);
 
-  const labels = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const options = {
+      acc[name][month]++;
+
+      return acc;
+    }, {} as Record<string, number[]>);
+  }, [jobsForYear]);
+
+  const latestMonth = useMemo(() => {
+    return jobsForYear.reduce((acc, curr) => {
+      const { created_at } = curr;
+      const month = new Date(created_at!).getMonth();
+
+      if (month > acc) {
+        acc = month;
+      }
+
+      return acc;
+    }, 0);
+  }, [jobsForYear]);
+
+  const options: ChartOptions<"bar"> = {
     plugins: {
       title: {
         display: true,
-        text: "Stacked Chart: Number of Jobs every month for 2022",
-      },
+        text: `Number of Jobs every month for ${year}`
+      }
     },
     responsive: true,
     scales: {
@@ -85,34 +102,33 @@ const JobsChart = () => {
         stacked: true,
         title: {
           display: true,
-          text: "Month",
-        },
+          text: "Months"
+        }
       },
       y: {
         stacked: true,
         title: {
           display: true,
-          text: "Number of Jobs",
-        },
-      },
-    },
+          text: "Number of Jobs"
+        }
+      }
+    }
   };
 
-  const data = {
-    labels: labels.slice(0, new Date(Date.now()).getMonth() + 1),
-    datasets: Object.entries(counts).map(([key, value], idx) => {
-      return {
-        label: key,
-        data: value,
-        backgroundColor: colorsList[idx],
-      };
-    }),
-  };
-  return (
-    <div>
-      <Bar data={data} options={options} />
-    </div>
-  );
+  const data = useMemo(() => {
+    return {
+      labels: MONTHS.slice(0, latestMonth + 1),
+      datasets: Object.entries(counts).map(([key, value], idx) => {
+        return {
+          label: key,
+          data: value,
+          backgroundColor: COLORS_LIST[idx]
+        };
+      })
+    };
+  }, [counts, latestMonth]);
+
+  return <Bar data={data} options={options} />;
 };
 
 export default JobsChart;
--- frontend/src/pages/billing.tsx
+++ frontend/src/pages/billing.tsx
@@ -1,16 +1,80 @@
-import { Container, Heading, Spacer } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  Input,
+  Select,
+  Spacer,
+  VStack
+} from "@chakra-ui/react";
 import { withAuthenticated } from "@/components/withAuthenticated";
 import JobsChart from "@/components/StackedChart";
+import { fetchJobs } from "@/data";
+import { Job } from "@/data/types";
+import { useState, useEffect, useMemo } from "react";
 
 function Billing() {
+  const [jobs, setJobs] = useState<Job[]>([]);
+
+  useEffect(() => {
+    const getJobs = async () => {
+      const data = await fetchJobs();
+      if (data) {
+        setJobs(data);
+      }
+    };
+    getJobs();
+  }, []);
+
+  const availableYears = useMemo(() => {
+    return Array.from(
+      new Set(
+        jobs.reduce((acc, curr) => {
+          const year = new Date(curr.created_at!).getFullYear();
+          if (!acc.includes(year)) {
+            acc.push(year);
+          }
+          return acc;
+        }, [] as number[])
+      )
+    );
+  }, [jobs]);
+
+  const defaultYear = useMemo(() => {
+    return availableYears[0];
+  }, [availableYears]);
+
+  const [year, setYear] = useState<number>();
+
+  const finalYear = year || defaultYear;
+
   return (
     <Container pt={8} maxW={"container.lg"} justifyContent={"center"}>
       <Heading lineHeight={1.5} textAlign={"center"}>
         Billing
       </Heading>
-      <Spacer height='20px' />
-      {/* <Text textAlign={"center"}>Coming soon...</Text> */}
-      <JobsChart />
+      <Spacer height="20px" />
+
+      <VStack spacing={4} alignItems={"flex-start"}>
+        <Heading>
+          Jobs for {finalYear} ({jobs.length})
+        </Heading>
+
+        <Select
+          marginX={8}
+          defaultValue={String(defaultYear)}
+          onChange={(e) => setYear(Number(e.target.value))}
+          placeholder="Select option"
+        >
+          {availableYears.map((year) => {
+            return (
+              <option selected={year === defaultYear} key={year} value={year}>
+                {year}
+              </option>
+            );
+          })}
+        </Select>
+        <JobsChart year={finalYear} jobs={jobs} />
+      </VStack>
     </Container>
   );
 }
