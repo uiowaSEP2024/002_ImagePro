60f6648e6e0883ac9f57a89602a5098de5db3bb0
Michal Brzus <michal-brzus@uiowa.edu>
2024-02-05 18:29:15 -0600

    ADD: add prototype of brainmask tool

--- example_tool/brainmask_tool.py
+++ example_tool/brainmask_tool.py
@@ -1,25 +1,64 @@
+#!/usr/bin/env python3
+import sys
 import argparse
 
+description = "author: Michal Brzus\nBrainmask Tool\n"
 
-def main():
-    parser = argparse.ArgumentParser(description='Process input and output directory paths.')
+# parse command line
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument(
+    "-s",
+    "--session_dir",
+    metavar="directory",
+    required=True,
+    help="Input dicom session directory",
+)
+parser.add_argument(
+    "-o",
+    "--output_dir",
+    metavar="directory",
+    required=True,
+    help="Output nifti directory",
+)
 
-    # Define the input directory argument
-    parser.add_argument('-i', '--input_dir_path', required=True,
-                        help='Path to the input directory')
-
-    # Define the output directory argument
-    parser.add_argument('-o', '--output_dir_path', required=True,
-                        help='Path to the output directory')
-
-    # Parse the arguments
-    args = parser.parse_args()
-
-    # You can now use args.input_dir_path and args.output_dir_path
-    # For example, print them:
-    print("Input Directory:", args.input_dir_path)
-    print("Output Directory:", args.output_dir_path)
+if len(sys.argv) == 1:
+    parser.print_help()
+    exit(1)
+args = parser.parse_args()
 
 
-if __name__ == "__main__":
-    main()
+from subprocess import run
+from pipeline_functions import *
+from pathlib import Path
+
+# create output directory
+if not Path(args.output_dir).exists():
+    print("Creating output directory")
+    run(["mkdir", "-p", args.output_dir])
+
+# run dicom inference and NIfTI conversion
+print("Processing DICOM data")
+nifti_path = dicom_inference_and_conversion(
+    session_dir=args.session_dir,
+    output_dir=args.output_dir,
+    model_path="./rf_dicom_modality_classifier.onnx",
+)
+print("NIfTI files created")
+
+# get NIfTI files
+raw_anat_nifti_files = list(Path(nifti_path).glob("*.nii.gz"))
+
+# create data dictionary for inference
+data_dict = [{"image": str(f)} for f in raw_anat_nifti_files]
+
+# create output directory for brainmasks
+brainmask_output_dir = Path(nifti_path).parent.as_posix() + "/brainmasks"
+if not Path(brainmask_output_dir).exists():
+    print("Creating brainmask output directory")
+    run(["mkdir", "-p", brainmask_output_dir])
+
+# run inference
+print("Running brainmask inference")
+brainmask_inference(data_dict, "brainmask_model.ckpt", brainmask_output_dir)
+
+
