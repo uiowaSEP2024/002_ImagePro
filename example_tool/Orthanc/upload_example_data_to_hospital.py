cd47f3615911a11d04dd20d4d230d1e0a65bee98
Ivan Johnson <iejohnson@uiowa.edu>
2024-02-13 18:03:13 -0600

    ENH: Simple script for uploading dataset into the Pacs Server

---
+++ example_tool/Orthanc/upload_example_data_to_hospital.py
@@ -0,0 +1,29 @@
+from pyorthanc import Orthanc
+import pydicom
+from pathlib import Path
+
+
+def upload_single_study_dir(orthanc: Orthanc, base_data_path: Path) -> None:
+    # study_instance_uid: str | None = None
+    for dicom_file in base_data_path.glob("**/*"):
+        if dicom_file.is_file():
+            try:
+                _ = pydicom.dcmread(dicom_file, stop_before_pixels=True)
+            except pydicom.errors.InvalidDicomError:
+                # Not dicom file
+                continue
+            with open(dicom_file, "rb") as file:
+                print(f"Pushing: {dicom_file}")
+                orthanc.post_instances(file.read())
+
+
+def do_main():
+    client_orthanc = Orthanc("http://127.0.0.1:8030")
+    file_path = Path(__file__)
+    example_tool_path = file_path.parent.parent
+    data_path = example_tool_path / "example_data/CAIPIRINHA_SPACE_Neuro_Head_Examples"
+    upload_single_study_dir(client_orthanc, data_path)
+
+
+if __name__ == "__main__":
+    do_main()
