79bc7a74e4fa9b62b32635021ee6063a43f5d2ff
Ivan Johnson <iejohnson@uiowa.edu>
2024-02-20 20:43:43 -0600

    DOC: Add Doc Strings for job_configuration.py

--- job_monitoring_app/backend/app/models/job_configuration.py
+++ job_monitoring_app/backend/app/models/job_configuration.py
@@ -6,6 +6,34 @@ from .base import Base, DateMixin
 
 
 class JobConfiguration(Base, DateMixin):
+    """
+    JobConfiguration model
+
+    A JobConfiguration represents a specific configuration for a job. It is created by a provider and
+    can be associated with multiple jobs. Each JobConfiguration is uniquely identified by a combination
+    of provider_id, tag, and version.
+
+    Attributes:
+    -----------
+    id : int
+        Auto-generated internal job configuration id
+    tag : str
+        Internal tag used to link a job configuration
+    name : str
+        The job configuration name as specified by the provider
+    provider_id : int
+        ForeignKey to User id of the provider
+    version : str
+        Version of the job configuration
+    # TODO Figure out exactly what a relationship means in this context and add a description
+    provider : relationship
+        Relationship to the User model representing the provider
+    step_configurations : relationship
+        Relationship to the StepConfiguration model. Represents all step configurations associated with this job configuration
+    jobs : relationship
+        Relationship to the Job model. Represents all jobs associated with this job configuration
+    """
+
     __tablename__ = "job_configurations"
     __table_args__ = (UniqueConstraint("provider_id", "tag", "version"),)
 
@@ -19,7 +47,9 @@ class JobConfiguration(Base, DateMixin):
     name = Column(String, nullable=False)
 
     # The provider creating the configuration
-    provider_id = Column(Integer, ForeignKey("users.id"), index=True, nullable=False)
+    provider_id: Column = Column(
+        Integer, ForeignKey("users.id"), index=True, nullable=False
+    )
 
     # version of the job configuration
     version = Column(String, index=True, nullable=False)
