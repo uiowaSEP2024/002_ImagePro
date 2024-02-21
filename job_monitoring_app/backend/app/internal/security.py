00fbfb0aabbf338fc1296860e988bb7566659433
Ivan Johnson <iejohnson@uiowa.edu>
2024-02-20 20:10:54 -0600

    DOC: Add Doc Strings for security.py

--- job_monitoring_app/backend/app/internal/security.py
+++ job_monitoring_app/backend/app/internal/security.py
@@ -5,7 +5,18 @@ from jose import jwt
 
 def create_access_token(
     data: dict, expires_minutes: float, secret: str, algorithm: str
-):
+) -> str:
+    """
+    Creates an access token
+
+    :param data: Data to be encoded in the token
+    :param expires_minutes: Time in minutes
+    :param secret: Secret key
+    :param algorithm: Algorithm to be used for encoding the token
+
+    :return: Encoded token
+    """
+    # This seems weird for it to have its very own file but it's not worth the effort to refactor
     to_encode = data.copy()
     expires_delta = timedelta(minutes=float(expires_minutes))
 
