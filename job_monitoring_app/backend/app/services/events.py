7804e91bbe97abe4c85486d60d4dc17121793cbc
Ivan Johnson <iejohnson@uiowa.edu>
2024-02-21 09:22:21 -0600

    DOC: ADD DocStrings to the events.py

--- job_monitoring_app/backend/app/services/events.py
+++ job_monitoring_app/backend/app/services/events.py
@@ -1,12 +1,27 @@
-from app import models, schemas
+from app import models, schemas  # TODO: Fix imports
 from sqlalchemy.orm import Session
 
 from .job_configuration import get_step_configuration_by_composite_key
 from .jobs import get_job_by_provider_job_id
 
+# TODO: @Zach - Can you help me understand the differn
 
-def create_event(db: Session, event: schemas.EventCreatePublic, provider):
-    job = get_job_by_provider_job_id(db, event.provider_job_id, provider.id)
+
+def create_event(
+    db: Session, event: schemas.EventCreatePublic, provider: models.Provider
+) -> models.Event:
+    """
+    Create an event for a job
+
+
+    Args:
+        db (Session): SQLAlchemy session
+        event (schemas.EventCreatePublic): Event to create
+        provider: Provider
+    Returns:
+        models.Event: Created event
+    """
+    job: models.job = get_job_by_provider_job_id(db, event.provider_job_id, provider.id)
 
     params = dict(
         kind=event.kind,
@@ -14,11 +29,16 @@ def create_event(db: Session, event: schemas.EventCreatePublic, provider):
         job_id=job.id,
         event_metadata=event.event_metadata,
     )
-
+    # If the event has a tag, we need to find the step_configuration_id???
+    # What is the purpose of the tag??
     if event.tag:
         step_configuration = get_step_configuration_by_composite_key(
             db, job.job_configuration_id, event.tag
         )
+        # TODO: Make note of how this is actually working and add it to the documentation
+        # This seems like a weird way to do this? Does it just update the dictionary?
+        # Why not just add the step_configuration_id to the dictionary in the first place?
+
         params.update(dict(step_configuration_id=step_configuration.id))
 
     db_event = models.Event(**params)
@@ -28,11 +48,32 @@ def create_event(db: Session, event: schemas.EventCreatePublic, provider):
     return db_event
 
 
-def get_event_by_id(db: Session, event_id: int):
+def get_event_by_id(db: Session, event_id: int) -> models.Event:
+    """
+    Get event by id from the database
+
+    Args:
+        db (Session): SQLAlchemy session
+        event_id (int): Event id (primary key)
+
+    Returns:
+        models.Event: Event
+    """
     return db.query(models.Event).filter(models.Event.id == event_id).first()
 
 
-def update_event(db: Session, event: schemas.EventUpdate):
+def update_event(db: Session, event: schemas.EventUpdate) -> models.Event:
+    """
+    Update an event in the database
+    Belive this is used to update the status of each step in a workflow job
+    # TODO: @Zach - Can you confirm this? and provide more details on the use case for this function?
+
+    Args:
+        db (Session): SQLAlchemy session
+        event (schemas.EventUpdate): Event to update
+    returns:
+        models.Event: The Updated event
+    """
     db_update_event = get_event_by_id(db, event.id)
 
     db_update_event.kind = event.kind
