c22faf9a201bc101f1da75715ce55a81d14151ca
Ivan Johnson <iejohnson@uiowa.edu>
2024-02-20 18:31:57 -0600

    BUG: Fix Merge Conflicts

--- job_monitoring_app/backend/app/schemas/__init__.py
+++ job_monitoring_app/backend/app/schemas/__init__.py
@@ -1,16 +1,7 @@
 from app.schemas.api_key import *
-<<<<<<< HEAD
 from app.schemas.event import *
 from app.schemas.job import *
 from app.schemas.job_configuration import *
 from app.schemas.metadata_configuration import *
 from app.schemas.step_configuration import *
 from app.schemas.user import *
-=======
-from app.schemas.event import Event, EventCreatePublic, EventPure, EventUpdate
-from app.schemas.job import Job, JobCreate, JobPure
-from app.schemas.job_configuration import JobConfiguration, JobConfigurationCreate
-from app.schemas.metadata_configuration import MetadataConfigurationCreate
-from app.schemas.step_configuration import StepConfigurationCreate, StepConfiguration
-from app.schemas.user import User, UserCreate
->>>>>>> 5fd766ca (BUG: Fixed a schema file and some lines that got commented out)
