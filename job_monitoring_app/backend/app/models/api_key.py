55741173f78e543baad08bf623854012735aec45
Ivan Johnson <iejohnson@uiowa.edu>
2024-02-20 20:20:53 -0600

    DOC: Add Doc Strings for api_key.py and base.py

--- job_monitoring_app/backend/app/models/api_key.py
+++ job_monitoring_app/backend/app/models/api_key.py
@@ -5,6 +5,27 @@ from .base import Base, DateMixin
 
 
 class Apikey(Base, DateMixin):
+    """
+    Apikey model
+
+    Attributes:
+    -----------
+    id : int
+        The primary key of the api key
+    user_id : int
+        The foreign key of the user
+    key : str
+        The api key
+    note : str
+        A note about the api key ??
+        Seems more like a title or description
+        it is required to make the api key from the frontend but not required to be unique
+    expires_at : datetime
+        The expiration date of the api key
+    user : User
+        The user that the api key belongs to
+    """
+
     __tablename__ = "api_keys"
 
     id = Column(Integer, primary_key=True, index=True)
