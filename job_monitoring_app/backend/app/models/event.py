f065ec36861730adc7c8ee0f5ae3dc4cddd1c62f
Ivan Johnson <iejohnson@uiowa.edu>
2024-02-20 20:22:07 -0600

    DOC: Add Doc Strings for event.py and refactored to get rid of warnings

--- job_monitoring_app/backend/app/models/event.py
+++ job_monitoring_app/backend/app/models/event.py
@@ -7,14 +7,37 @@ from .base import DateMixin, Base
 
 
 class Event(Base, DateMixin):
+    """
+    Event model
+
+    Attributes:
+    -----------
+    id : int
+        The primary key of the event
+    job_id : int
+        The foreign key of the job
+    event_name : str
+        The name of the event
+    kind : str
+        The kind of event (info, step, error, etc.)
+    event_metadata : dict
+        The event metadata (e.g. the error message or the step number)
+        ?? Are these the only two things we use to populate the event metadata ??
+    """
+
     __tablename__ = "events"
 
     # Auto-generated internal event id
     id = Column(Integer, primary_key=True, index=True)
 
     # The job id the events correspond to
-    job_id = Column(Integer, ForeignKey("jobs.id"), index=True, nullable=False)
-    job = relationship("Job", back_populates="events", foreign_keys=[job_id])
+    job = relationship(
+        "Job",
+        back_populates="events",
+        foreign_keys=[
+            Column(Integer, ForeignKey("jobs.id"), index=True, nullable=False)
+        ],
+    )
 
     # The event_name as specified by the provider e.g. "Scanning X"
     name = Column(String, nullable=True)
@@ -25,17 +48,17 @@ class Event(Base, DateMixin):
         nullable=False,
     )
 
-    step_configuration_id = Column(
-        Integer,
-        ForeignKey("step_configurations.id", ondelete="SET NULL"),
-        index=True,
-        nullable=True,
-    )
-
     step_configuration = relationship(
         "StepConfiguration",
         back_populates="events",
-        foreign_keys=[step_configuration_id],
+        foreign_keys=[
+            Column(
+                Integer,
+                ForeignKey("step_configurations.id", ondelete="SET NULL"),
+                index=True,
+                nullable=True,
+            )
+        ],
     )
 
     # The event metadata
