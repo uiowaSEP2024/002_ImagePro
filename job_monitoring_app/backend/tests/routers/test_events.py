96bfcc2569500aaef147e8b371dd0bfb79c458f7
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-02-22 15:47:49 -0600

    ENH: Modified tests to properly compare update time, modified update_event in services to properly update event_metadata

--- job_monitoring_app/backend/tests/routers/test_events.py
+++ job_monitoring_app/backend/tests/routers/test_events.py
@@ -1,3 +1,5 @@
+import datetime
+
 from app import services
 from app.dependencies import API_KEY_HEADER_NAME
 
@@ -32,3 +34,74 @@ def test_create_event(app_client, job_for_random_user_with_api_key, db):
         ).id
     )
     assert response.json()["created_at"] is not None
+
+
+def test_update_event(app_client, job_for_random_user_with_api_key, db):
+    # Create an event
+    job = job_for_random_user_with_api_key
+    event_data = {
+        "kind": "step",
+        "name": "Scanning",
+        "provider_job_id": job.provider_job_id,
+        "event_metadata": {"official": "Yes"},
+    }
+    response = app_client.post(
+        "/events",
+        json=event_data,
+        headers={
+            API_KEY_HEADER_NAME: job_for_random_user_with_api_key.provider.api_keys[
+                0
+            ].key
+        },
+    )
+    assert response.status_code == 200
+
+    # Retrieve the created event ID
+    event_id = response.json()["id"]
+
+    # Capture current time before update
+    current_time_before_update = datetime.datetime.utcnow()
+
+    # Prepare data for updating the event
+    updated_event_data = {
+        "kind": "complete",
+        "id": event_id,
+        "event_metadata": {"Reason": "None"},
+    }
+
+    # Send update request
+    update_response = app_client.post(
+        "/update_event",
+        json=updated_event_data,
+        headers={
+            API_KEY_HEADER_NAME: job_for_random_user_with_api_key.provider.api_keys[
+                0
+            ].key
+        },
+    )
+
+    # Retrieve the updated events update time
+    update_datetime = datetime.datetime.strptime(
+        update_response.json()["updated_at"], "%Y-%m-%dT%H:%M:%S.%f%z"
+    )
+    update_datetime = update_datetime.replace(tzinfo=None)
+
+    # Assert update was successful
+    assert update_response.status_code == 200
+
+    # Capture current time after update
+    current_time_after_update = datetime.datetime.utcnow()
+
+    # # Verify the updated event attributes
+
+    assert update_response.json()["kind"] == updated_event_data["kind"]
+    assert update_response.json()["id"] == updated_event_data["id"]
+    assert (
+        update_response.json()["event_metadata"] == updated_event_data["event_metadata"]
+    )
+    assert update_response.json()["name"] == event_data["name"]
+
+    assert update_response.json()["job_id"] == job.id
+    assert update_response.json()["created_at"] == response.json()["created_at"]
+
+    assert current_time_before_update <= update_datetime <= current_time_after_update
