a30bae2cedbfc069b9d4954e6abfb76bdd62ddcb
Ivan Johnson <iejohnson@uiowa.edu>
2024-02-23 10:48:53 -0600

    DOC: Adding JS Docs withAuthenticated.tsx

--- job_monitoring_app/frontend/src/components/withAuthenticated.tsx
+++ job_monitoring_app/frontend/src/components/withAuthenticated.tsx
@@ -6,9 +6,14 @@ import {
 } from "@/hooks/useAuthContext";
 
 /**
- * A HOC that ensures the user is authenticated before rendering the wrapped component
- * @param WrappedComponent
- * @returns
+ * A Higher Order Component (HOC) that ensures the user is authenticated before rendering the wrapped component.
+ * If the user is not authenticated, it will not render the wrapped component.
+ * It also supports role-based authentication by accepting an array of roles as an optional parameter.
+ * Only users with a role that is included in the roles array will be able to access the wrapped component.
+ *
+ * @param WrappedComponent - The component to be wrapped by the HOC.
+ * @param roles - An optional array of user roles. If provided, only users with a role that is included in this array will be able to access the wrapped component.
+ * @returns - A new component that wraps the original component with authentication logic.
  */
 export const withAuthenticated = <P extends object>(
   WrappedComponent: React.ComponentType<P>,
@@ -25,9 +30,11 @@ export const withAuthenticated = <P extends object>(
 };
 
 /**
- * A HOC that ensures the user is un-authenticated before rendering the wrapped component
- * @param WrappedComponent
- * @returns
+ * A Higher Order Component (HOC) that ensures the user is unauthenticated before rendering the wrapped component.
+ * If the user is authenticated, it will not render the wrapped component.
+ *
+ * @param WrappedComponent - The component to be wrapped by the HOC.
+ * @returns - A new component that wraps the original component with authentication logic.
  */
 export const withUnauthenticated = <P extends object>(
   WrappedComponent: React.ComponentType<P>
