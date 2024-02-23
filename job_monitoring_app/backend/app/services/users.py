3b2719876c8d3654390b4c4ce882f87573a98c51
Ivan Johnson <iejohnson@uiowa.edu>
2024-02-22 19:24:27 -0600

    DOC: Adding DocStrings to users.py

--- job_monitoring_app/backend/app/services/users.py
+++ job_monitoring_app/backend/app/services/users.py
@@ -6,15 +6,39 @@ from app.internal import get_password_hash, verify_password
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def get_user(db: Session, user_id: int):
+def get_user(db: Session, user_id: int) -> models.User:
+    """
+    Retrieves a user from the database by ID.
+
+    Args:
+        db (Session): The database session.
+        user_id (int): The ID of the user to retrieve.
+
+    Returns:
+        models.User: The user retrieved from the database.
+    """
     return db.query(models.User).get(user_id)
 
 
-def get_user_by_email(db: Session, email: str):
+def get_user_by_email(db: Session, email: str) -> models.User:
+    """
+    Retrieves a user from the database by email.
+
+    Args:
+        db (Session): The database session.
+        email (str): The email of the user to retrieve.
+    """
     return db.query(models.User).filter(models.User.email == email).first()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
+def create_user(db: Session, user: schemas.UserCreate) -> models.User:
+    """
+    Creates a new user in the database.
+
+    Args:
+        db (Session): The database session.
+        user (schemas.UserCreate): The user to create.
+    """
     db_user = models.User(
         email=user.email,
         hashed_password=get_password_hash(user.password),
@@ -28,9 +52,19 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 
-def authenticate_user(db: Session, username: str, password: str):
+def authenticate_user(db: Session, username: str, password: str) -> models.User:
+    """
+    Authenticates a user by username and password.
+
+    Args:
+        db (Session): The database session.
+        username (str): The username of the user to authenticate.
+        password (str): The password of the user to authenticate.
+
+    """
+
     user = get_user_by_email(email=username, db=db)
-    print(user)
+    # print(user)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
