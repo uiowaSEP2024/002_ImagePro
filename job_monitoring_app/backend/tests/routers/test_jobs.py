85904853c658f31cd6f159e5e53afec0bf2697e4
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-02-29 17:16:39 -0600

    FIX: Changed job to study in backend router tests, changed customer to hospital

--- job_monitoring_app/backend/tests/routers/test_jobs.py
+++ job_monitoring_app/backend/tests/routers/test_jobs.py
@@ -2,14 +2,14 @@ from app import services, schemas
 from app.dependencies import API_KEY_HEADER_NAME
 
 
-def test_create_job(
+def test_create_study(
     app_client, random_provider_user_with_api_key, random_job_configuration_factory
 ):
     job_configuration = random_job_configuration_factory.get()
 
     data = {
-        "provider_job_id": "2432424",
-        "customer_id": random_provider_user_with_api_key.id,
+        "provider_study_id": "2432424",
+        "hospital_id": random_provider_user_with_api_key.id,
         "tag": job_configuration.tag,
     }
 
@@ -22,13 +22,13 @@ def test_create_job(
     )
 
     assert response.status_code == 200
-    assert response.json()["provider_job_id"] == "2432424"
-    assert response.json()["customer_id"] == random_provider_user_with_api_key.id
+    assert response.json()["provider_study_id"] == "2432424"
+    assert response.json()["hospital_id"] == random_provider_user_with_api_key.id
     assert response.json()["created_at"] is not None
     assert response.json()["job_configuration_id"] == job_configuration.id
 
 
-def test_get_job_as_customer(
+def test_get_study_as_hospital(
     app_client,
     db,
     random_provider_user_with_api_key,
@@ -37,18 +37,18 @@ def test_get_job_as_customer(
 ):
     job_configuration = random_job_configuration_factory.get()
 
-    job = services.create_job(
+    study = services.create_study(
         db,
-        schemas.JobCreate(
-            provider_job_id="145254",
-            customer_id=random_test_user.id,
+        schemas.StudyCreate(
+            provider_study_id="145254",
+            hospital_id=random_test_user.id,
             tag=job_configuration.tag,
         ),
         provider=random_provider_user_with_api_key,
     )
 
     db.commit()
-    db.refresh(job)
+    db.refresh(study)
 
     # Simulate user log in
     response = app_client.post(
@@ -58,20 +58,20 @@ def test_get_job_as_customer(
     # Grab access token for user
     access_token = response.json()["access_token"]
 
-    # Use access token in the request to get a job
+    # Use access token in the request to get a study
     response = app_client.get(
-        f"/studies/{job.id}", cookies={"access_token": access_token}
+        f"/studies/{study.id}", cookies={"access_token": access_token}
     )
 
     assert response.status_code == 200
-    assert response.json()["id"] == job.id
-    assert response.json()["customer_id"] == job.customer_id
-    assert response.json()["provider_id"] == job.provider_id
+    assert response.json()["id"] == study.id
+    assert response.json()["hospital_id"] == study.hospital_id
+    assert response.json()["provider_id"] == study.provider_id
     assert response.json()["created_at"] is not None
     assert response.json()["job_configuration_id"] == job_configuration.id
 
 
-def test_get_jobs_as_customer(
+def test_get_studies_as_hospital(
     app_client,
     db,
     random_provider_user_with_api_key,
@@ -80,29 +80,29 @@ def test_get_jobs_as_customer(
 ):
     job_configuration = random_job_configuration_factory.get()
 
-    job1 = services.create_job(
+    study1 = services.create_study(
         db,
-        schemas.JobCreate(
-            provider_job_id="145254",
-            customer_id=random_test_user.id,
+        schemas.StudyCreate(
+            provider_study_id="145254",
+            hospital_id=random_test_user.id,
             tag=job_configuration.tag,
         ),
         provider=random_provider_user_with_api_key,
     )
 
-    job2 = services.create_job(
+    study2 = services.create_study(
         db,
-        schemas.JobCreate(
-            provider_job_id="145255",
-            customer_id=random_test_user.id,
+        schemas.StudyCreate(
+            provider_study_id="145255",
+            hospital_id=random_test_user.id,
             tag=job_configuration.tag,
         ),
         provider=random_provider_user_with_api_key,
     )
 
     db.commit()
-    db.refresh(job1)
-    db.refresh(job2)
+    db.refresh(study1)
+    db.refresh(study2)
 
     # Simulate user log in
     response = app_client.post(
@@ -112,72 +112,72 @@ def test_get_jobs_as_customer(
     # Grab access token for user
     access_token = response.json()["access_token"]
 
-    # Use access token in the request to get a job
+    # Use access token in the request to get a study
     response = app_client.get("/studies", cookies={"access_token": access_token})
 
     assert response.status_code == 200
     assert len(response.json()) == 2
 
-    assert response.json()[0]["id"] == job1.id
-    assert response.json()[0]["customer_id"] == job1.customer_id
-    assert response.json()[0]["provider_id"] == job1.provider_id
+    assert response.json()[0]["id"] == study1.id
+    assert response.json()[0]["hospital_id"] == study1.hospital_id
+    assert response.json()[0]["provider_id"] == study1.provider_id
     assert response.json()[0]["created_at"] is not None
 
-    assert response.json()[1]["id"] == job2.id
-    assert response.json()[1]["customer_id"] == job2.customer_id
-    assert response.json()[1]["provider_id"] == job2.provider_id
+    assert response.json()[1]["id"] == study2.id
+    assert response.json()[1]["hospital_id"] == study2.hospital_id
+    assert response.json()[1]["provider_id"] == study2.provider_id
     assert response.json()[1]["created_at"] is not None
 
 
-def test_get_job_as_different_customer(
+def test_get_study_as_different_hospital(
     app_client,
     db,
     random_provider_user_with_api_key,
     random_test_user_factory,
     random_job_configuration_factory,
 ):
-    customer_a = random_test_user_factory.get()
-    customer_b = random_test_user_factory.get()
+    hospital_a = random_test_user_factory.get()
+    hospital_b = random_test_user_factory.get()
 
     job_configuration = random_job_configuration_factory.get()
 
-    job = services.create_job(
+    study = services.create_study(
         db,
-        schemas.JobCreate(
-            provider_job_id="145254",
-            customer_id=customer_a.id,
+        schemas.StudyCreate(
+            provider_study_id="145254",
+            hospital_id=hospital_a.id,
             tag=job_configuration.tag,
         ),
         provider=random_provider_user_with_api_key,
     )
 
     db.commit()
-    db.refresh(job)
+    db.refresh(study)
 
-    # Simulate user log in as customer b (different customer from the
-    # one who the job was made for)
+    # Simulate user log in as hospital b (different hospital from the
+    # one who the study was made for)
     response = app_client.post(
-        "/login", data={"username": customer_b.email, "password": "abc"}
+        "/login", data={"username": hospital_b.email, "password": "abc"}
     )
 
-    # Grab access token for the different customer
+    # Grab access token for the different hospital
     access_token = response.json()["access_token"]
 
-    # Use access token in the request to get a job
+    # Use access token in the request to get a study
     response = app_client.get(
-        f"/studies/{job.id}", cookies={"access_token": access_token}
+        f"/studies/{study.id}", cookies={"access_token": access_token}
     )
 
     # Response should be rejected
     assert response.status_code == 403
 
 
-def test_create_job_with_missing_tag(
+def test_create_study_with_missing_tag(
     app_client, random_provider_user_with_api_key, random_job_configuration_factory
 ):
     data = {
-        "provider_job_id": "2432424",
-        "customer_id": random_provider_user_with_api_key.id,
+        "provider_study_id": "2432424",
+        "hospital_id": random_provider_user_with_api_key.id,
     }
 
     response = app_client.post(
