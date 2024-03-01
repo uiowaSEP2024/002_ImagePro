76d4aa8464949be9bedad0ff9d51c5561c1f5056
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-03-01 16:56:05 -0600

    ENH: Modified test event models to have study tests, failing until we complete job to study transition

--- job_monitoring_app/backend/tests/models/test_events.py
+++ job_monitoring_app/backend/tests/models/test_events.py
@@ -17,10 +17,22 @@ def test_create_event(db, random_test_user, random_provider_user):
     db.commit()
     db.refresh(job)
 
+    # TODO take out job stuff above after job code is removed
+    study = models.Study(
+        provider_study_id="abc123",
+        provider_study_name="kidneyV1",
+        hospital_id=random_test_user.id,
+        provider_id=random_provider_user.id,
+    )
+    db.add(study)
+    db.commit()
+    db.refresh(study)
+
     # Create events for the job
 
     event = models.Event(
-        job_id=job.id,
+        job_id=job.id,  # TODO take out after job code is removed
+        study_id=study.id,
         name="Scanning Kidney",
         kind="Pending",
         event_metadata={"unofficial": "Yes"},
@@ -29,19 +41,23 @@ def test_create_event(db, random_test_user, random_provider_user):
     db.add(event)
     db.commit()
     db.refresh(event)
-    db.refresh(job)
+    db.refresh(job)  # TODO take out after job code is removed
+    db.refresh(study)
 
-    assert event.job_id == job.id
+    assert event.job_id == job.id  # TODO take out after job code is removed
+    assert event.study_id == study.id
     assert event.kind == "Pending"
     assert event.name == "Scanning Kidney"
     assert event.event_metadata == {"unofficial": "Yes"}
     assert event.created_at is not None
 
-    assert len(job.events) == 1
-    assert event.job.id == job.id
+    assert len(job.events) == 1  # TODO take out after job code is removed
+    assert len(study.events) == 1
+    assert event.job.id == job.id  # TODO take out after job code is removed
+    assert event.study.id == study.id
 
 
-def test_create_event_missing_job_id(db):
+def test_create_event_missing_job_id(db):  # TODO take out after job code is removed
     event = models.Event(name="Scanning Kidney", kind="Pending")
 
     db.add(event)
@@ -53,7 +69,21 @@ def test_create_event_missing_job_id(db):
     assert "job_id" in str(exc.value.orig)
 
 
-def test_create_job_missing_kind(db, random_test_user, random_provider_user):
+def test_create_event_missing_study_id(db):
+    event = models.Event(name="Scanning Kidney", kind="Pending")
+
+    db.add(event)
+
+    with pytest.raises(sqlalchemy.exc.IntegrityError) as exc:
+        db.commit()
+
+    assert isinstance(exc.value.orig, psycopg2.errors.NotNullViolation)
+    assert "study_id" in str(exc.value.orig)
+
+
+def test_create_job_missing_kind(
+    db, random_test_user, random_provider_user
+):  # TODO take out after job code is removed
     job = models.Job(
         provider_job_name="kidneyV1",
         customer_id=random_test_user.id,
@@ -70,7 +100,24 @@ def test_create_job_missing_kind(db, random_test_user, random_provider_user):
     assert "provider_job_id" in str(exc.value.orig)
 
 
-def test_create_job_missing_provider_job_name(
+def test_create_study_missing_kind(db, random_test_user, random_provider_user):
+    study = models.Study(
+        provider_study_name="kidneyV1",
+        hospital_id=random_test_user.id,
+        provider_id=random_provider_user.id,
+    )
+
+    db.add(study)
+
+    with pytest.raises(sqlalchemy.exc.IntegrityError) as exc:
+        db.commit()
+
+    # Check for null violation and that column is part of the error message in the error
+    assert isinstance(exc.value.orig, psycopg2.errors.NotNullViolation)
+    assert "provider_study_id" in str(exc.value.orig)
+
+
+def test_create_job_missing_provider_job_name(  # TODO take out after job code is removed
     db, random_test_user, random_provider_user
 ):
     job = models.Job(
@@ -97,27 +144,54 @@ def test_create_job_missing_provider_job_name(
     assert "kind" in str(exc.value.orig)
 
 
+def test_create_study_missing_provider_study_name(
+    db, random_test_user, random_provider_user
+):
+    study = models.Study(
+        provider_study_id="abc123",
+        provider_study_name="kidneyV1",
+        hospital_id=random_test_user.id,
+        provider_id=random_provider_user.id,
+    )
+
+    db.add(study)
+    db.commit()
+    db.refresh(study)
+
+    # Create events for the job
+
+    event = models.Event(study_id=study.id, name="Scanning Kidney")
+
+    db.add(event)
+
+    with pytest.raises(sqlalchemy.exc.IntegrityError) as exc:
+        db.commit()
+
+    assert isinstance(exc.value.orig, psycopg2.errors.NotNullViolation)
+    assert "kind" in str(exc.value.orig)
+
+
 def test_create_event_for_step(
     db, random_test_user, random_provider_user, random_job_configuration_factory
 ):
     job_configuration = random_job_configuration_factory.get(num_steps=1)
 
-    job = models.Job(
-        provider_job_id="abc123",
-        provider_job_name="kidneyV1",
-        customer_id=random_test_user.id,
+    study = models.Study(
+        provider_study_id="abc123",
+        provider_study_name="kidneyV1",
+        hospital_id=random_test_user.id,
         provider_id=random_provider_user.id,
         job_configuration_id=job_configuration.id,
     )
 
-    db.add(job)
+    db.add(study)
     db.commit()
-    db.refresh(job)
+    db.refresh(study)
 
-    # Create events for the job
+    # Create events for the study
 
     event = models.Event(
-        job_id=job.id,
+        study_id=study.id,
         name="Scanning Kidney",
         kind="Pending",
         step_configuration_id=job_configuration.step_configurations[0].id,
@@ -126,6 +200,6 @@ def test_create_event_for_step(
     db.add(event)
     db.commit()
     db.refresh(event)
-    db.refresh(job)
+    db.refresh(study)
 
     assert event.step_configuration_id == job_configuration.step_configurations[0].id
