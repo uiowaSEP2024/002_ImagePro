51ed0d24bc00424e2762b7bb8d03c5d725512fc5
Michal Brzus <michal-brzus@uiowa.edu>
2024-02-29 19:06:15 -0600

    FIX: add missing study id input

--- example_tool/run_example_data.sh
+++ example_tool/run_example_data.sh
@@ -2,8 +2,10 @@
 # from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )  # Get the directory of the script
 brains_tool_path="${SCRIPT_DIR}/brainmask_tool.py"  # Get the path to the brains_tool.py
+example_id="abc123"  # Set the example_id
 example_data_path="${SCRIPT_DIR}/example_data/CAIPIRINHA_SPACE_Neuro_Head_Examples"  # Get the path to the example_data
 example_output_path="${SCRIPT_DIR}/example_output"  # Get the path to the example_output
 python3 "${brains_tool_path}" \
+-i "${example_id}" \
 -s "${example_data_path}" \
 -o "${example_output_path}"\
