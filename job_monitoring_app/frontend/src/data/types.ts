e1a1080ea4df528bcc583d53c3ec5180c003d4fc
powersaudrey25 <powersaudrey25@gmail.com>
2024-03-04 13:58:22 -0600

    FIX: misspelling and missed references

--- job_monitoring_app/frontend/src/data/types.ts
+++ job_monitoring_app/frontend/src/data/types.ts
@@ -4,4 +4,4 @@ export type User = components["schemas"]["User"];
 export type UserCreate = components["schemas"]["UserCreate"];
 export type Study = components["schemas"]["Study"];
 export type ApiKey = components["schemas"]["ApikeyPublic"];
-export type JobEvent = components["schemas"]["Event"];
+export type StudyEvent = components["schemas"]["Event"];
