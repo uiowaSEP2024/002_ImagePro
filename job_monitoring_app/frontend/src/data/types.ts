e64d5ddd78d049a8e5f6facecd18fc19f87d8b6f
powersaudrey25 <powersaudrey25@gmail.com>
2024-03-04 12:45:24 -0600

    ENH: update frontend data to use study

--- job_monitoring_app/frontend/src/data/types.ts
+++ job_monitoring_app/frontend/src/data/types.ts
@@ -2,6 +2,6 @@ import { components } from "./schema";
 
 export type User = components["schemas"]["User"];
 export type UserCreate = components["schemas"]["UserCreate"];
-export type Job = components["schemas"]["Job"];
+export type Study = components["schemas"]["Study"];
 export type ApiKey = components["schemas"]["ApikeyPublic"];
 export type JobEvent = components["schemas"]["Event"];
