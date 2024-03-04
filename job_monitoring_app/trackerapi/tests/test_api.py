a790800eebb213e120d2076b32ab3662a98c251f
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-03-04 12:42:59 -0600

    FIX: Changed trackerapi tests to from jobs to studies

--- job_monitoring_app/trackerapi/tests/test_api.py
+++ job_monitoring_app/trackerapi/tests/test_api.py
@@ -16,29 +16,29 @@ def test_init_without_api_key():
         _ = TrackerApi()
 
 
-def test_requests_made_with_api_key():
+def test_study_requests_made_with_api_key():
     with requests_mock.Mocker() as m:
         tracker = TrackerApi(api_key="abc123", skip_verify=True)
 
         m.post(
-            tracker.urls.jobs_url,
+            tracker.urls.studies_url,
             json={
-                "provider_job_id": "1",
-                "customer_id": 1,
-                "provider_job_name": "test_job",
+                "provider_study_id": "1",
+                "hospital_id": 1,
+                "provider_study_name": "test_study",
             },
         )
 
-        tracker.create_job("1", 1, "test_job")
+        tracker.create_study("1", 1, "test_study")
 
         assert (
             tracker.api_key
             == m.last_request.headers[TrackerApi.HTTP_API_KEY_HEADER_KEY]
-        ), "Expected api_key to be sent on request to create job"
+        ), "Expected api_key to be sent on request to create study"
 
 
 @responses.activate
-def test_job():
+def test_study():
     tracker = TrackerApi(
         api_key="abc123", base_url=TrackerApi.DEFAULT_BASE_URL, skip_verify=True
     )
@@ -52,16 +52,18 @@ def test_job():
     # TODO: make meaningful assertion about the job config once implemented
 
     responses.add(
-        url=tracker.urls.jobs_url, method=responses.POST, json={"provider_job_id": "1"}
+        url=tracker.urls.studies_url,
+        method=responses.POST,
+        json={"provider_study_id": "1"},
     )
 
-    tracker_jobapi = tracker.create_job("1", 1, "test_job")
+    tracker_study_api = tracker.create_study("1", 1, "test_study")
     assert (
-        tracker_jobapi.provider_job_id == "1"
-    ), "Expected TrackerJobApi to set the returned job_provider_id on itself"
+        tracker_study_api.provider_study_id == "1"
+    ), "Expected TrackerStudyApi to set the returned study_provider_id on itself"
 
     responses.add(url=tracker.urls.events_url, method=responses.POST, json={"id": 1})
-    tracker_eventapi = tracker_jobapi.send_event(kind="Pending", tag="step_1")
+    tracker_eventapi = tracker_study_api.send_event(kind="Pending", tag="step_1")
     assert (
         tracker_eventapi.event_id == 1
     ), "Expected TrackerEventApi to set the returned event id on itself"
@@ -75,7 +77,7 @@ def test_job():
             "event_metadata": {"Reason": "None"},
         },
     )
-    tracker_update_event_api = tracker_jobapi.update_event(
+    tracker_update_event_api = tracker_study_api.update_event(
         kind="Complete", event_id=1, metadata={"Reason": "None"}
     )
     assert (
