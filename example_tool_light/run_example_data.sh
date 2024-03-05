b6ea737e459958f298638c9a2633a2cd61a1668b
Michal Brzus <michal-brzus@uiowa.edu>
2024-03-05 15:10:13 -0600

    FIX: update bash scrpt for the new data organization

--- example_tool_light/run_example_data.sh
+++ example_tool_light/run_example_data.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )  # Get the directory of the script
+BASE_DIR="$(dirname ${SCRIPT_DIR})"  # Get base directory path
 brains_tool_path="${SCRIPT_DIR}/brainmask_tool.py"  # Get the path to the brains_tool.py
-example_data_path="${SCRIPT_DIR}/example_data/CAIPIRINHA_SPACE_Neuro_Head_Examples"  # Get the path to the example_data
+example_data_path="${BASE_DIR}/example_data/CAIPIRINHA_SPACE_Neuro_Head_Examples"  # Get the path to the example_data
 example_output_path="${SCRIPT_DIR}/example_output"  # Get the path to the example_output
 python3 "${brains_tool_path}" \
--i "skjsdkhsd" \
+-i "abc123" \
 -s "${example_data_path}" \
 -o "${example_output_path}"\
