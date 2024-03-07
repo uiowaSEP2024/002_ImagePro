5c13507d72deec48dbdd90680750cbc2bf1c6b51
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-03-07 11:43:08 -0600

    ADD: Create services file for pacs db functions

---
+++ job_monitoring_app/backend/app/services/pacs.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from app import models, schemas
+
+
+def create_hospital(db: Session, pacs: schemas.PacsCreate) -> models.Pacs:
+    """
+    Creates a new PACS in the database.
+
+    Args:
+        db (Session): The database session.
+        PACS (schemas.PacsCreate): The Pacs to create.
+    """
+
+    db_pacs = models.Pacs(pacs_name=pacs.pacs_name, hospital_id=pacs.hospital_id)
+    db.add(db_pacs)
+    db.commit()
+    db.refresh(db_pacs)
+    return db_pacs
