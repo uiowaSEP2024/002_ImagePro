f51fa44c90c4b8dfe3b55ff728a4861dd0fb482f
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-03-07 15:54:38 -0600

    ADD: Create product model test file

---
+++ job_monitoring_app/backend/tests/models/test_products.py
@@ -0,0 +1,35 @@
+from config import config
+from app import models
+
+from app.schemas import ProductCreate
+from app.services.products import create_product
+
+
+def test_create_products():
+    db = config.db.SessionLocal()
+
+    # Create a new provider
+
+    provider = models.Provider(provider_name="test_provider")
+    db.add(provider)
+    db.commit()
+    db.refresh(provider)
+
+    # Create a new product
+
+    db_product = create_product(
+        db,
+        ProductCreate.parse_obj(
+            {
+                "product_name": "test_product",
+                "provider_id": provider.id,
+            }
+        ),
+    )
+
+    assert db_product.product_name == "test_product"
+    assert db_product.provider_id == provider.id
+    assert db_product.created_at is not None
+
+    assert len(provider.products) == 1
+    assert db_product.provider.id == provider.id
