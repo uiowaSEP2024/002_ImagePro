70997899a1befa86a5ec236007f2528269e98f30
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-03-07 11:34:01 -0600

    ADD: Created PACS model file

---
+++ job_monitoring_app/backend/app/models/PACS.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String, Integer
+from sqlalchemy.orm import relationship
+
+from .base import Base, DateMixin
+
+
+class PACS(Base, DateMixin):
+    """
+    A PACS is a server used by hospitals to send and receive studies
+    PACS communicate with products???
+
+    #TODO @Ivan is this correct?
+    Attributes:
+    -----------
+    id: int
+        Auto-generated internal hospital id
+    pacs_name: str
+        A name identifier given to the PACS
+    hospital_id: int
+        The hospital id that the PACS is connected to
+
+
+    """
+
+    __tablename__ = "pacs"
+
+    # Auto-generated internal PACS id
+    id: Column = Column(Integer, primary_key=True, index=True)
+
+    # A name identifier given to the PACS
+    pacs_name: Column = Column(String, index=True, nullable=False)
+
+    # The hospital id that the PACS is connected to
+    hospital_id: Column = Column(
+        Integer, ForeignKey("hospitals.id"), index=True, nullable=False
+    )
+
+    hospital = relationship(
+        "Hospital",
+        back_populates="pacs",
+        foreign_keys=[
+            hospital_id,
+        ],
+    )
