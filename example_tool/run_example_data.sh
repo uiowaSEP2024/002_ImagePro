11ddf8f799b6fde42ed2da134ee522a832775ee5
Michal Brzus <michal-brzus@uiowa.edu>
2024-03-18 20:00:44 -0500

    BUG: Fix output path on exicutatable
    Signed-off-by: Michal Brzus <44876465+mbrzus@users.noreply.github.com>
    Signed-off-by: Ivan Johnson <106177326+ivan-e-johnson@users.noreply.github.com>
    Signed-off-by: Audrey Powers <57544141+powersaudrey25@users.noreply.github.com>

--- example_tool/run_example_data.sh
+++ example_tool/run_example_data.sh
@@ -5,7 +5,7 @@ BASE_DIR="$( dirname "${SCRIPT_DIR}" )"  # Get base directory path
 brains_tool_path="${SCRIPT_DIR}/brainmask_tool.py"  # Get the path to the brains_tool.py
 example_id="abc123"  # Set the example_id
 example_data_path="${BASE_DIR}/example_data/CAIPIRINHA_SPACE_Neuro_Head_Examples"  # Get the path to the example_data
-example_output_path="${SCRIPT_DIR}/example_output"  # Get the path to the example_output
+example_output_path="${SCRIPT_DIR}/example_output/sub-12345"  # Get the path to the example_output
 python3 "${brains_tool_path}" \
 -i "${example_id}" \
 -s "${example_data_path}" \
