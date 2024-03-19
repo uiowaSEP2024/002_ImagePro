a142034b3a80a94b6dd0e79f50056122f4abc7c4
Michal Brzus <michal-brzus@uiowa.edu>
2024-03-19 09:55:20 -0500

    ENH: update paths in example tool light bash script

--- example_tool_light/run_example_data.sh
+++ example_tool_light/run_example_data.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) 
 BASE_DIR="$( dirname "${SCRIPT_DIR}" )"  # Get base directory path
 brains_tool_path="${SCRIPT_DIR}/brainmask_tool.py"  # Get the path to the brains_tool.py
 example_data_path="${BASE_DIR}/example_data/CAIPIRINHA_SPACE_Neuro_Head_Examples"  # Get the path to the example_data
-example_output_path="${SCRIPT_DIR}/example_output"  # Get the path to the example_output
+example_output_path="${SCRIPT_DIR}/example_output/sub-12345"
 python3 "${brains_tool_path}" \
 -i "abc123" \
 -s "${example_data_path}" \
