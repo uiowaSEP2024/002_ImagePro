55ad8a5c9abeb33b0a031658fb6353aabeb9fbc9
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-03-26 14:14:45 -0500

    ADD: Create migration for user_provider join table

--- job_monitoring_app/backend/app/models/provider_users.py
+++ job_monitoring_app/backend/app/models/provider_users.py
@@ -1,16 +1,23 @@
-# from sqlalchemy import Column, Integer, ForeignKey, Table
-# from sqlalchemy.ext.declarative import declarative_base
-#
-# from .user import User
-# from .provider import Provider
-#
-#
-# Base = declarative_base()
-#
-# # Association table
-# provider_user_association = Table(
-#     "provider_user",
-#     Base.metadata,
-#     Column("user_id", Integer, ForeignKey(User.id), primary_key=True),
-#     Column("provider_id", Integer, ForeignKey(Provider.id), primary_key=True),
-# )
+from sqlalchemy import Column, Integer, ForeignKey, Table
+from sqlalchemy.ext.declarative import declarative_base
+
+from .user import User
+from .provider import Provider
+
+
+Base = declarative_base()
+
+# Association table
+provider_user_association = Table(
+    "user_provider",
+    Base.metadata,
+    Column(
+        "user_id", Integer, ForeignKey(User.id, ondelete="CASCADE"), primary_key=True
+    ),
+    Column(
+        "provider_id",
+        Integer,
+        ForeignKey(Provider.id, ondelete="CASCADE"),
+        primary_key=True,
+    ),
+)
