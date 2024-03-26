9bf15f74b02a0f58786a3329951c94a11993523b
powersaudrey25 <powersaudrey25@gmail.com>
2024-03-25 19:18:24 -0500

    ENH: join table migration

---
+++ job_monitoring_app/backend/alembic/versions/f69b9a500cee_create_user_hospital_join_table.py
@@ -0,0 +1,30 @@
+"""create user hospital join table
+
+Revision ID: f69b9a500cee
+Revises: 7c316d7dd01f
+Create Date: 2024-03-25 18:13:20.499715
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "f69b9a500cee"
+down_revision = "7c316d7dd01f"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "user_hospital",
+        sa.Column("user_id", sa.Integer(), sa.ForeignKey("users.id"), primary_key=True),
+        sa.Column(
+            "hospital_id", sa.Integer(), sa.ForeignKey("hospitals.id"), primary_key=True
+        ),
+    )
+
+
+def downgrade():
+    op.drop_table("user_hospital")
