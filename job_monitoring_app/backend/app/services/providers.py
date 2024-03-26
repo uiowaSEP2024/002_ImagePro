0993d7d01aa6b0a8aee541363cc72ba49bd6bfe4
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-03-26 14:26:16 -0500

    ENH: Implemented provider functionality

--- job_monitoring_app/backend/app/services/providers.py
+++ job_monitoring_app/backend/app/services/providers.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
 from app import models, schemas
 
+from app.models.provider_users import provider_user_association
+
 
 def create_provider(db: Session, provider: schemas.ProviderCreate) -> models.Provider:
     """
@@ -19,12 +21,16 @@ def create_provider(db: Session, provider: schemas.ProviderCreate) -> models.Pro
 
 
 def get_provider_users(db: Session, provider_id: int) -> models.User:
-    pass
-    # """
-    # Get all users in a provider.
-    #
-    # Args:
-    #     db (Session): The database session.
-    #     provider_id (int): The provider id.
-    # """
-    # return db.query(models.User).filter(models.User.provider_id == provider_id).all()
+    """
+    Get all users in a provider.
+
+    Args:
+        db (Session): The database session.
+        provider_id (int): The provider id.
+    """
+    return (
+        db.query(models.User)
+        .join(provider_user_association)
+        .filter(provider_user_association.c.provider_id == provider_id)
+        .all()
+    )
