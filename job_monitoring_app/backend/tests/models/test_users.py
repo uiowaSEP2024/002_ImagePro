64d06920deb48f452fd0489573a6deb979fef544
Zachary Morris <zachary-j-morris@uiowa.edu>
2024-03-26 13:09:51 -0500

    ENH: Created function to get a user's connected hospital, created tests for it

--- job_monitoring_app/backend/tests/models/test_users.py
+++ job_monitoring_app/backend/tests/models/test_users.py
@@ -3,8 +3,13 @@ import sqlalchemy
 
 from config import config
 from app.schemas import UserCreate
+from app.schemas import UserHospitalCreate
 from app.services.users import create_user
+from app.services.users import create_hospital_user
 from app.schemas.user import UserRoleEnum
+from app.schemas.hospital import HospitalCreate
+from app.services.hospitals import create_hospital
+from app.services.users import get_user_hospital
 
 
 def test_users_no_role():
@@ -123,3 +128,39 @@ def test_unique_user_email():
                 }
             ),
         )
+
+
+def test_users_hospital_association():
+    db = config.db.SessionLocal()
+
+    db_hospital = create_hospital(
+        db,
+        HospitalCreate.parse_obj(
+            {
+                "hospital_name": "test_hospital_bobby",
+            }
+        ),
+    )
+
+    db = config.db.SessionLocal()
+    db_user = create_hospital_user(
+        db,
+        UserHospitalCreate.parse_obj(
+            {
+                "email": "zzz@example.com",
+                "password": "abc",
+                "first_name": "Bobby",
+                "last_name": "Busche",
+                "role": UserRoleEnum.hospital,
+                "hospital_id": db_hospital.id,
+            }
+        ),
+    )
+
+    assert db_user.email == "zzz@example.com"
+    assert db_user.hashed_password is not None
+    assert db_user.first_name == "Bobby"
+    assert db_user.last_name == "Busche"
+    assert db_user.role == UserRoleEnum.hospital
+    assert db_user.created_at is not None
+    assert get_user_hospital(db, db_user.id).hospital_name == db_hospital.hospital_name
