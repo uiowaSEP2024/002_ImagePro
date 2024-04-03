2d9c5b3cd839c1f1e1322e81ec5ac3f164f97c1e
powersaudrey25 <powersaudrey25@gmail.com>
2024-04-02 21:50:59 -0500

    ADD: providers router

--- job_monitoring_app/backend/app/routers/providers.py
+++ job_monitoring_app/backend/app/routers/providers.py
@@ -1,9 +1,10 @@
 from typing import List
 
-from app import schemas, services
 from app.dependencies import get_db
+from app import schemas, services
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+
 
 router = APIRouter()
 router.tags = ["providers"]
@@ -14,3 +15,11 @@ def get_providers(
     db: Session = Depends(get_db),
 ):
     return services.get_all_providers(db)
+
+
+@router.get("/providers/{provider_id}", response_model=schemas.Provider)
+def get_provider(
+    provider_id: int,
+    db: Session = Depends(get_db),
+):
+    return services.get_provider_by_id(db, provider_id=provider_id)
