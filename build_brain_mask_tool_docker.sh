624cc37c3f00fd9f1c47b4869490deda3fc45554
iejohnson <iejohnson@uiowa.edu>
2024-04-15 13:46:17 -0500

    ENH: Update to build and push to EKS

--- build_brain_mask_tool_docker.sh
+++ build_brain_mask_tool_docker.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-
 # Define variables
 DOCKERFILE_NAME="Dockerfile_BrainMaskTool"
-IMAGE_TAG="brainmasktool:v0.1"
+IMAGE_TAG="brainmasktool"
 IMAGE_LABEL="brainmasktool"
+AWS_REGION="us-east-1"
+AWS_ACCOUNT_ID=325852638497
+REPOSITORY_NAME="fargate_test"
 
 # Build the docker image for the brain mask tool
-docker build --tag "${IMAGE_TAG}" --label "${IMAGE_LABEL}" --file "${DOCKERFILE_NAME}" "$(pwd)"
+docker build --tag "${IMAGE_TAG}" --label "${IMAGE_LABEL}" --file "${DOCKERFILE_NAME}" .
+
+# If building for Kubernetes, tag the image with the ECR registry
+BUILD_FOR_KUBERNETES=${BUILD_FOR_KUBERNETES:-"1"}
+REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY_NAME}"
+
+if [ "$BUILD_FOR_KUBERNETES" = "1" ]; then
+    # AWS ECR login
+    aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${REGISTRY}
+
+    # Tag the image for ECR with a versioned tag
+    docker tag "${IMAGE_TAG}" "${REGISTRY}:${IMAGE_TAG}"
+
+    # Push the image to AWS ECR
+    docker push "${REGISTRY}:${IMAGE_TAG}"
+fi
