cd82d6cba18a555fb4b9fd386a56a454a553bf2b
iejohnson <iejohnson@uiowa.edu>
2024-04-19 12:40:39 -0500

    WIP: Some starter files for HERE files and base sh scripts
    the goal is to source aws_common_resources to dry out code
    and to reduce liklyhood of naming missmatch

---
+++ aws_cluster/aws_common_resources.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# Common Environment Variables for AWS Cluster
+# This should allow us to dry out our scripts and only have to source this file
+
+CURRENT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CLUSTER_DIR="$(dirname "${CURRENT_DIR}"))"
+
+## PATHS FOR JOB MONITORING APP
+JOB_MONITORING_DIR="${CLUSTER_DIR}/job_monitoring_app"
+FRONTEND_DIR="${JOB_MONITORING_DIR}/frontend"
+BACKEND_DIR="${JOB_MONITORING_DIR}/backend"
+TRACKER_API_DIR="${JOB_MONITORING_DIR}/tracker_api"
+
+## DOCKERFILES FOR JOB MONITORING APP
+# Should we update the naming convention for the Dockerfiles?
+FRONTEND_DOCKERFILE="${CURRENT_DIR}/frontend/Dockerfile_Frontend_aws"
+# TODO - Update the Dockerfile for the backend once we move it to the cluster directory
+BACKEND_DOCKERFILE="${CLUSTER_DIR}/Dockerfile_Backend"
+
+
+## PATHS FOR ORTHANC and AGENT
+
+ORTHANC_DIR="${CLUSTER_DIR}/example_tool/Orthanc"
+AGENT_DIR="${CLUSTER_DIR}/internal_servers
+
+
+
+## AWS CONFIGURATION
+AWS_REGION="us-east-1"
+AWS_ACCOUNT_ID=325852638497
+REPOSITORY_NAME="manual_gui_ecr"
