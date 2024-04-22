eb7bb349284bd1fe0a5a1519d4e144bf3a63a2dc
iejohnson <iejohnson@uiowa.edu>
2024-04-22 12:38:41 -0500

    BUG: Fix incorrect platfrom for building

--- aws_cluster/build_and_push_to_ecr.sh
+++ aws_cluster/build_and_push_to_ecr.sh
@@ -40,20 +40,14 @@ case $BUILD_CHOICE in
         ;;
 esac
 
-# Build the docker image for the brain mask tool
-docker build --tag "${IMAGE_TAG}" --label "${IMAGE_LABEL}" --file "${DOCKERFILE_NAME}" "${CONTEXT_DIR}"
-
-# If building for Kubernetes, tag the image with the ECR registry
-BUILD_FOR_KUBERNETES=${BUILD_FOR_KUBERNETES:-"1"}
 REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY_NAME}"
+# Build the docker image for the brain mask tool
+docker buildx build --platform linux/amd64,linux/arm64 --tag "${IMAGE_TAG}" --label "${IMAGE_LABEL}" --file "${DOCKERFILE_NAME}" "${CONTEXT_DIR}"
+# AWS ECR login
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${REGISTRY}
 
-if [ "$BUILD_FOR_KUBERNETES" = "1" ]; then
-    # AWS ECR login
-    aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${REGISTRY}
+# Tag the image for ECR with a versioned tag
+docker tag "${IMAGE_TAG}" "${REGISTRY}:${IMAGE_TAG}"
 
-    # Tag the image for ECR with a versioned tag
-    docker tag "${IMAGE_TAG}" "${REGISTRY}:${IMAGE_TAG}"
-
-    # Push the image to AWS ECR
-    docker push "${REGISTRY}:${IMAGE_TAG}"
-fi
+# Push the image to AWS ECR
+docker push "${REGISTRY}:${IMAGE_TAG}"
