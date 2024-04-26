a248d6449f18aa71a4b2bed8786a22eec00b1cfe
powersaudrey25 <powersaudrey25@gmail.com>
2024-04-26 00:29:14 -0500

    ADD: new orthanc specifically for minikube, tmux just for hospital
    I had to update the config file so that it is able to talk to the hospital that will be hosted locally

---
+++ example_tool/Orthanc/tmux_startup_only_hospital.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# If you don't have tmux installed check the following link:
+# https://github.com/tmux/tmux/wiki/Installing
+orthanc_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Prompt the user if they want to kill all Orthanc processes
+echo "Do you want to kill all Orthanc processes before starting? (y/n)"
+read -r kill_answer
+if [ "${kill_answer}" = "y" ]; then
+    echo "Killing all Orthanc processes..."
+    killall Orthanc || echo "No Orthanc processes were found running."
+fi
+
+# Start TMUX session
+tmux new-session -d -s example_tool_testing
+
+# Create windows for running hospital and internal PACS
+tmux new-window -d -t example_tool_testing -n run_hospital
+tmux send-keys -t example_tool_testing:run_hospital.0 "/bin/bash $(pwd)/run_hospital.sh" C-m
+
+#tmux new-window -d -t example_tool_testing -n run_internal_pacs
+#tmux send-keys -t example_tool_testing:run_internal_pacs.0 "/bin/bash $(pwd)/run_internal_pacs.sh" C-m
+
+# Prompt for uploading example data
+echo "Do you want to upload example data? (y/n)"
+read -r upload_answer
+if [ "${upload_answer}" = "y" ]; then
+  # Create a new window for uploading data
+  tmux new-window -d -t example_tool_testing -n upload_data
+  tmux send-keys -t example_tool_testing:upload_data.0 "python3 '${orthanc_dir}/upload_example_data_to_hospital.py'" C-m
+fi
+
+# Attach to the session
+tmux attach -t example_tool_testing
